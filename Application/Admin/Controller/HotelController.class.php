55d0a6934ab6a9c630a6dba3af05c472124eac8c
hongwei <208109721@qq.com>
2017-01-13 10:02:56 +0800

    '修改表字段'

--- Application/Admin/Controller/HotelController.class.php
+++ Application/Admin/Controller/HotelController.class.php
@@ -103,7 +103,7 @@ class HotelController extends BaseController
 		$save                        = [];
 		$save['name']                = I('post.name','','trim');
 		$save['addr']                = I('post.addr','','trim');
-		$save['contactor']           = I('post.contactor','','trim');
+		$save['contractor']          = I('post.contractor','','trim');
 		$save['maintainer']          = I('post.maintainer','','trim');
 		$save['tel']                 = I('post.tel','','trim');
 		$save['level']               = I('post.level','','trim');
