553825a2b865fff61e936ba0c54a404325727afb
hongwei <208109721@qq.com>
2017-01-16 09:33:19 +0800

    '整合OSS'

---
+++ Application/Admin/Controller/VersionController.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *@author hongwei
+ *
+ *
+ * 
+ */
+namespace Admin\Controller;
+
+use Admin\Controller\BaseController;
+
+class VersionController extends BaseController 
+{
+
+
+	public function client()
+	{
+
+		return $this->display('client');
+	}
+
+
+
+
+
+
+
+
+
+}//End Class
