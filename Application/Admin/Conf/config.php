e2787a8d54eecf49e053714693eab505cb11d083
hongwei <208109721@qq.com>
2017-01-17 10:44:29 +0800

    '修改异步通知接口路径'

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -22,7 +22,7 @@ $config = array(
 	'OSS_ACCESS_KEY'  => 'sv8aZCKEJhQ0nwKHj8uEnw3ADwcM24',
 	'OSS_ENDPOINT'    => 'oss-cn-hangzhou.aliyuncs.com',  //注意不要在前面加 http:// 
 	'OSS_TEST_BUCKET' => '219bucket',                     //资源空间,即桶
-	'OSS_SYNC_CALLBACK_URL'=>'http://devp.savorx.cn/admin/send/syncCallback', //上传异步回调地址
+	'OSS_SYNC_CALLBACK_URL'=>'http://devp.savorx.cn/admin/notify/syncCallback', //上传异步回调地址
 	/***********OSSS上传配置**********/
 	
 
