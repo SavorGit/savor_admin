dbb46f85ea27a8aad147e9e553e4eea5caacdbd8
xiaobaig0407 <xiaobaig0407@163.com>
2017-01-22 21:44:35 +0800

    添加节目节点所需model类分别对应menu_hotel,menu_list,menu_listlog表

---
+++ application/admin/Model/MenuHotelModel.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *酒店model
+ *@author  hongwei <[<email address>]>
+ * 
+ */
+namespace Admin\Model;
+
+use Common\Lib\Page;
+use Admin\Model\BaseModel;
+
+class MenuHotelModel extends BaseModel
+{
+	protected $tableName='menu_hotel';
+	public function getWhere($where, $order, $field){
+
+		$list = $this->where($where)->order($order)->field($field)->select();
+
+		return $list;
+	}
+
+
+	public function getList($where, $order='id desc', $start=0,$size=5)
+	{	
+		
+		 $list = $this->where($where)
+					  ->order($order)
+					  ->limit($start,$size)
+					  ->select();
+
+		
+		$count = $this->where($where)
+					  ->count();
+
+		$objPage = new Page($count,$size);		  
+
+		$show = $objPage->admin_page();
+
+		$data = array('list'=>$list,'page'=>$show);
+
+        return $data;
+
+
+	}//End Function
+
+
+
+
+
+	
+
+
+
+
+}//End Class
---
+++ application/admin/Model/MenuItemModel.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *酒店model
+ *@author  hongwei <[<email address>]>
+ * 
+ */
+namespace Admin\Model;
+
+use Common\Lib\Page;
+use Admin\Model\BaseModel;
+
+class MenuItemModel extends BaseModel
+{
+
+	public function getWhere($where, $order, $field){
+
+		$list = $this->where($where)->order($order)->field($field)->select();
+
+
+		return $list;
+	}
+
+
+	public function getList($where, $order='id desc', $start=0,$size=5)
+	{	
+		
+		 $list = $this->where($where)
+					  ->order($order)
+					  ->limit($start,$size)
+					  ->select();
+
+		
+		$count = $this->where($where)
+					  ->count();
+
+		$objPage = new Page($count,$size);		  
+
+		$show = $objPage->admin_page();
+
+		$data = array('list'=>$list,'page'=>$show);
+
+        return $data;
+
+
+	}//End Function
+
+
+
+
+
+	
+
+
+
+
+}//End Class
