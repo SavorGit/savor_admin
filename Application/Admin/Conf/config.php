355755bbbf4012482a7e4013e415bc849b9166a3
liubin0125 <liubin-0125@163.com>
2017-02-06 17:49:40 +0800

    修改数据库配置到config_test。线上相关配置为config_online

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,30 +3,5 @@
 $config = array(
     'VAR_PAGE'=>'pageNum',
     'SHOW_PAGE_TRACE'=>false,
-   
-   //数据库配置信息
-	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => '127.0.0.1', // 服务器地址
-	'DB_NAME'   => 'savoradmin', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => 'root', // 密码
-	'DB_PORT'   => 3306, // 端口
-	'DB_PARAMS' =>  array(), // 数据库连接参数
-	'DB_PREFIX' => 'savor_', // 数据库表前缀 
-	'DB_CHARSET'=> 'utf8', // 字符集
-	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
-
-
-	/***********OSSS上传配置**********/
-	'OSS_ACCESS_ID'   => 'tnDh4AQqRYbV9mq8',
-	'OSS_ACCESS_KEY'  => 'sv8aZCKEJhQ0nwKHj8uEnw3ADwcM24',
-	'OSS_ENDPOINT'    => 'oss-cn-hangzhou.aliyuncs.com',  //注意不要在前面加 http:// 
-	'OSS_TEST_BUCKET' => '219bucket',                     //资源空间,即桶
-	'OSS_SYNC_CALLBACK_URL'=>'http://devp.savorx.cn/admin/notify/syncCallback', //上传异步回调地址
-	/***********OSSS上传配置**********/
-	
-
 );
-
-
 return $config;
--- Application/Common/Conf/config_test.php
+++ Application/Common/Conf/config_test.php
@@ -3,7 +3,7 @@ $DB_MASTER_HOST = 'localhost';
 $DB_SLAVE_HOST  = 'localhost';
 $db_name = 'savoradmin';
 $db_user = 'root';
-$db_pwd = '';
+$db_pwd = 'root';
 
 //redis缓存配置
 $redis['db1']['0']['host'] = '';
@@ -12,7 +12,6 @@ $redis['db1']['0']['isMaster'] = '1';
 $redis['db1']['1']['host'] = ''; 
 $redis['db1']['1']['port'] = '';
 $redis['db1']['1']['isMaster'] = '0';
-
 
 $config_db =  array(
 	'DB_DEPLOY_TYPE' => 1, //数据库主从支持
@@ -25,8 +24,17 @@ $config_db =  array(
     'DB_PORT' => 3306,
     'DB_CHARSET' => 'UTF8',
     'DB_PREFIX' => 'savor_',
+    'DB_DEBUG'  =>  TRUE,
 
  	'REDIS_CONFIG' => $redis,
+    
+    //OSSS上传配置
+    'OSS_ACCESS_ID'   => 'tnDh4AQqRYbV9mq8',
+    'OSS_ACCESS_KEY'  => 'sv8aZCKEJhQ0nwKHj8uEnw3ADwcM24',
+    'OSS_ENDPOINT'    => 'oss-cn-hangzhou.aliyuncs.com',  //注意不要在前面加 http://
+    'OSS_TEST_BUCKET' => '219bucket',                     //资源空间,即桶
+    'OSS_SYNC_CALLBACK_URL'=>'http://devp.savorx.cn/admin/notify/syncCallback', //上传异步回调地址
+    //end
 );
 return $config_db;
 
