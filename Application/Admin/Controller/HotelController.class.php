48277c1ae69c9d21d5726cb5bc47eba754156dd9
liubin0125 <liubin-0125@163.com>
2017-02-09 17:21:16 +0800

    修改酒楼管理宣传片以及tv,box相关

--- Application/Admin/Controller/HotelController.class.php
+++ Application/Admin/Controller/HotelController.class.php
@@ -8,138 +8,10 @@
 namespace Admin\Controller;
 
 use Admin\Controller\BaseController;
-
-use Admin\Model\AdsModel;
-use Admin\Model\HotelModel;
-use Admin\Model\AreaModel;
-use Admin\Model\RoomModel;
-
 class HotelController extends BaseController {
     public function __construct() {
         parent::__construct();
     }
-
-	/*
-	 * 添加宣传片media_1,media_2;
-	 */
-
-	public function addpub(){
-		//acctype=0 添加 ，acctype=1修改
-		$acctype = I('get.acctype');
-
-		$hoid = I('get.id');
-		$id = 1485;
-		$hotelModel = new HotelModel;
-
-		$hoinfo = $hotelModel->where('id='.$id)->find();
-		if($acctype == 1) {
-			$ads_id = I('get.adsid');
-			$adsModel = new AdsModel();
-			$vainfo = $adsModel->find($ads_id);
-			$this->assign('vainfo', $vainfo);
-		}
-		$this->assign('vinfo',$hoinfo);
-
-		$this->display('addpub');
-	}
-
-	/*
-	 * 对宣传片添加或者修改
-	 */
-	public function doAddPub(){
-		$adsModel = new AdsModel();
-		$id                  = I('post.ads_id');
-		$save                = [];
-		$save['description']        = I('post.descri');
-		$save['name']    = I('post.adsname');
-		//视频封面mediaid
-		$media_a   = I('post.media_id');
-		$mediaModel = new \Admin\Model\MediaModel();
-		$oss_arr = $mediaModel->find($media_a);
-		$oss_addr = $oss_arr['oss_addr'];
-		$save['duration'] = $oss_arr['oss_addr'];
-		$image_host = 'http://'.C('OSS_BUCKET').'.'.C('OSS_HOST').'/';
-		$oss_addr = $image_host.$oss_addr;
-		$save['img_url'] = $oss_addr;
-
-		//视频资源mediaid
-		$save['media_id']    = I('post.media_id');
-
-		$save['hotel_id']    = I('post.ho_id');
-		if($id){
-			$res_save = $adsModel->where('id='.$id)->save($save);
-			if($res_save){
-				 $this->output('操作成功!', 'hotel/doAddPub');
-			}else{
-				 $this->output('操作失败!', 'hotel/doAddPub');
-			}
-		}else{
-			$save['create_time'] = date('Y-m-d H:i:s');
-			$save['type'] = 3;
-			//刷新页面，关闭当前
-			$res_save = $adsModel->add($save);
-			if($res_save){
-				ob_clean();
-				 $this->output('添加宣传片成功!', 'hotel/pubmanager');
-			}else{
-				 $this->output('操作失败!', 'hotel/doAddPub');
-			}
-		}
-	}
-
-
-	/*
-	 * 宣传片列表
-	 */
-	public function pubmanager() {
-		var_dump($_REQUEST);
-		$hoid = I('hotel_id');
-		$hotelModel = new HotelModel;
-		$hoinfo = $hotelModel->find($hoid);
-		var_dump($hoinfo);
-		$this->assign('vainfo',$hoinfo);
-
-		$adsModel = new AdsModel();
-		$size   = I('numPerPage',50);//显示每页记录数
-		$this->assign('numPerPage',$size);
-		$start = I('pageNum',1);
-		$this->assign('pageNum',$start);
-		$order = I('_order','id');
-		$this->assign('_order',$order);
-		$sort = I('_sort','desc');
-		$this->assign('_sort',$sort);
-		$orders = $order.' '.$sort;
-		$start  = ( $start-1 ) * $size;
-
-		$where = "1=1";
-
-		$name = I('name');
-
-		if($name)
-		{
-			$this->assign('name',$name);
-			$where .= "	AND name LIKE '%{$name}%'";
-		}
-
-
-		$where .= "	AND hotel_id =  $hoid";
-		var_dump($where);
-
-
-
-		$result = $adsModel->getList($where,$orders,$start,$size);
-
-
-		$this->assign('list', $result['list']);
-		$this->assign('page',  $result['page']);
-		$this->display('pubmanager');
-	}
-
-	public function delpub(){
-		$ads_id = I('get.$ads_id');
-		//判断节目单是否含有ads_id
-		
-	}
 
     /**
      * 酒店列表
@@ -332,19 +204,144 @@ class HotelController extends BaseController {
 		$RoomModel = new \Admin\Model\RoomModel();
 		if($id){
 			if($RoomModel->where('id='.$id)->save($save)){
-				$this->output('操作成功!', 'hotel/room?hotel_id='.$hotel_id,2);
+				$this->output('操作成功!', 'hotel/room');
 			}else{
 				 $this->output('操作失败!', 'hotel/doAddRoom');
 			}		
 		}else{	
 			$save['create_time'] = date('Y-m-d H:i:s');
 			if($RoomModel->add($save)){
-				$this->output('操作成功!', 'hotel/room?hotel_id='.$hotel_id);
+				$this->output('操作成功!', 'hotel/room');
 			}else{
 				 $this->output('操作失败!', 'hotel/doAddRoom');
 			}	
 		}		
 	}
 
+	/*
+	 * 宣传片列表
+	 */
+	public function pubmanager() {
+	    $hotel_id= I('hotel_id');
+	    $size   = I('numPerPage',50);//显示每页记录数
+	    $name = I('keywords','','trim');
+	    $beg_time = I('begin_time','');
+	    $end_time = I('end_time','');
+	    $this->assign('numPerPage',$size);
+	    $start = I('pageNum',1);
+	    $this->assign('pageNum',$start);
+	    $order = I('_order','id');
+	    $this->assign('_order',$order);
+	    $sort = I('_sort','desc');
+	    $this->assign('_sort',$sort);
+	    $orders = $order.' '.$sort;
+	    $start  = ( $start-1 ) * $size;
+	    $where = "1=1";
+	    if($hotel_id)   $where .= "	AND hotel_id =  $hotel_id";
+	    if($name)   $where.= "	AND name LIKE '%{$name}%'";
+	    if($beg_time)   $where.=" AND create_time>='$beg_time'";
+	    if($end_time)   $where.=" AND create_time<='$end_time'";
+	
+	    $hotelModel = new \Admin\Model\HotelModel();
+	    $hotelinfo = $hotelModel->find($hotel_id);
+	    $adsModel = new \Admin\Model\AdsModel();
+	    $result = $adsModel->getList($where,$orders,$start,$size);
+	    $datalist = $result['list'];
+	    $mediaModel = new \Admin\Model\MediaModel();
+	    foreach ($datalist as $k=>$v){
+	        $media_id = $v['media_id'];
+	        if($media_id){
+	            $mediainfo = $mediaModel->getMediaInfoById($media_id);
+	            $oss_addr = $mediainfo['oss_addr'];
+	        }else{
+	            $oss_addr = '';
+	        }
+	        $datalist[$k]['oss_addr'] = $oss_addr;
+	    }
+	
+	    $time_info = array('now_time'=>date('Y-m-d H:i:s'),'begin_time'=>$beg_time,'end_time'=>$end_time);
+	    $this->assign('timeinfo',$time_info);
+	    $this->assign('keywords',$name);
+	    $this->assign('hotelinfo',$hotelinfo);
+	    $this->assign('list', $datalist);
+	    $this->assign('page',  $result['page']);
+	    $this->display('pubmanager');
+	}
+	
+	
+	/*
+	 * 添加宣传片
+	 */
+	public function addpub(){
+	    $hoid = I('get.hotel_id',0,'intval');
+	    $ads_id = I('get.ads_id',0,'intval');
+	    if($hoid){
+	        $hotelModel = new \Admin\Model\HotelModel();
+	        $hoinfo = $hotelModel->where('id='.$hoid)->find();
+	        $this->assign('vinfo',$hoinfo);
+	    }
+	    if($ads_id){
+	        $oss_host = 'http://'.C('OSS_BUCKET').'.'.C('OSS_HOST').'/';
+	        $adsModel = new \Admin\Model\AdsModel();
+	        $vainfo = $adsModel->find($ads_id);
+	        $vainfo['oss_addr'] = $oss_host.$vainfo['img_url'];
+	        if($vainfo['media_id']){
+	            $mediaModel = new \Admin\Model\MediaModel();
+	            $mediainfo = $mediaModel->getMediaInfoById($vainfo['media_id']);
+	            $vainfo['videooss_addr'] = $mediainfo['oss_addr'];
+	        }
+	        $this->assign('vainfo',$vainfo);
+	    }
+	    $this->display('addpub');
+	}
+	
+	/*
+	 * 对宣传片添加或者修改
+	 */
+	public function doAddPub(){
+	    $adsModel = new \Admin\Model\AdsModel();
+	    $mediaModel = new \Admin\Model\MediaModel();
+	    $ads_id = I('post.ads_id');
+	    $covermedia_id = I('post.covervideo_id','0','intval');//视频封面id
+	    $media_id = I('post.media_id','0','intval');//视频id
+	
+	    $save = [];
+	    $save['description'] = I('post.descri');
+	    $save['name'] = I('post.adsname');
+	    if($covermedia_id){
+	        $oss_arr = $mediaModel->find($covermedia_id);
+	        $oss_addr = $oss_arr['oss_addr'];
+	        $save['img_url'] = $oss_addr;
+	    }
+	    if($media_id){
+	        $oss_arr = $mediaModel->find($media_id);
+	        $save['duration'] = $oss_arr['duration'];
+	        $save['media_id']    = $media_id;
+	    }
+	    $save['hotel_id'] = I('post.hotel_id');
+	    if($ads_id){
+	        $res_save = $adsModel->where('id='.$ads_id)->save($save);
+	        if($res_save){
+	            $this->output('操作成功!', 'hotel/pubmanager');
+	        }else{
+	            $this->output('操作失败!', 'hotel/doAddPub');
+	        }
+	    }else{
+	        $save['create_time'] = date('Y-m-d H:i:s');
+	        $save['type'] = 3;
+	        //刷新页面，关闭当前
+	        $res_save = $adsModel->add($save);
+	        if($res_save){
+	            $this->output('添加宣传片成功!', 'hotel/pubmanager');
+	        }else{
+	            $this->output('操作失败!', 'hotel/doAddPub');
+	        }
+	    }
+	}
+	
+	public function delpub(){
+	    $ads_id = I('get.ads_id');
+	    $this->output('白玉涛开发', 'hotel/pubmanager');
+	}
 
 }
--- Application/Admin/Model/TvModel.class.php
+++ Application/Admin/Model/TvModel.class.php
@@ -10,31 +10,19 @@ use Common\Lib\Page;
 use Admin\Model\BaseModel;
 use Admin\Model\BoxModel;
 
-class TvModel extends BaseModel
-{
-
-	public function getList($where, $order='id desc', $start=0,$size=5)
-	{	
-		
+class TvModel extends BaseModel{
+	public function getList($where, $order='id desc', $start=0,$size=5){	
 		 $list = $this->where($where)
 					  ->order($order)
 					  ->limit($start,$size)
 					  ->select();
-
-		
 		$count = $this->where($where)
 					  ->count();
-
 		$objPage = new Page($count,$size);		  
-
 		$show = $objPage->admin_page();
-
 		$data = array('list'=>$list,'page'=>$show);
-
         return $data;
-
-
-	}//End Function
+	}
 
 
 
@@ -43,45 +31,26 @@ class TvModel extends BaseModel
 	 * @param  array  $result [description]
 	 * @return [type]         [description]
 	 */
-	public function boxIdToBoxName($result=[])
-	{
-		if(!$result || !is_array($result))
-		{
+	public function boxIdToBoxName($result=[]){
+		if(!$result || !is_array($result)){
 			return [];
 		}
-
 		$boxId = [];
-
-		foreach ($result as $value) 
-		{
+		foreach ($result as $value){
 			$boxId[] = $value['box_id'];
 		}
-
 		$filter       = [];
 		$filter['id'] = ['IN',$boxId];
-
 		$boxModel = new BoxModel;
-		
 		$arrBox = $boxModel->getAll('id,name',$filter);
-		
-		foreach ($result as &$value) 
-		{
-			foreach ($arrBox as  $row) 
-			{
-				if($value['box_id'] == $row['id'])
-				{
+		foreach ($result as &$value){
+			foreach ($arrBox as  $row){
+				if($value['box_id'] == $row['id']){
 					$value['box_name'] = $row['name'];
 				}
 			}
 		}
-		
 		return $result;
+	}
 
-	}//End Function
-
-
-
-
-
-
-}//End Class
+}
