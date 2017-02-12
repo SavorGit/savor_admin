3a96421cba98b53e8623cc0e810823dee827a538
liubin0125 <liubin-0125@163.com>
2017-02-12 19:59:11 +0800

    版本加入md5值

--- Application/Admin/Controller/VersionController.class.php
+++ Application/Admin/Controller/VersionController.class.php
@@ -5,6 +5,7 @@
 namespace Admin\Controller;
 
 use Admin\Controller\BaseController;
+use Common\Lib\Aliyun;
 class VersionController extends BaseController{
     private $oss_host = '';
     public function __construct(){
@@ -269,12 +270,16 @@ class VersionController extends BaseController{
 	        if($remark)    $version_data['remark'] = $remark;
 	        if($oss_addr){
 	            $version_data['oss_addr'] = $oss_addr;
-	            /*
-	            $file_url = $this->oss_host.$oss_addr;
-	            $file_info = file_get_contents($file_url);
-	            $md5_file = md5_file($file_info);
-	            $version_data['md5'] = $md5_file;
-	            */
+	            $accessKeyId = C('OSS_ACCESS_ID');
+	            $accessKeySecret = C('OSS_ACCESS_KEY');
+	            $endpoint = C('OSS_HOST');
+	            $bucket = C('OSS_BUCKET');
+	            $aliyun = new Aliyun($accessKeyId, $accessKeySecret, $endpoint);
+	            $aliyun->setBucket($bucket);
+	            $fileinfo = $aliyun->getObject($oss_addr,'');
+	            if($fileinfo){
+	                $version_data['md5'] = md5($fileinfo);
+	            }
 	        }
 	        $versionModel = new \Admin\Model\VersionModel();
 	        $res_version = $versionModel->add($version_data);
