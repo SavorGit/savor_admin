9e5689d601a86be1c25c426de13c5ca5cf6eb2ad
liubin0125 <liubin-0125@163.com>
2017-02-15 01:25:15 +0800

    修改包间名称不显示

--- Application/Admin/Model/BoxModel.class.php
+++ Application/Admin/Model/BoxModel.class.php
@@ -10,79 +10,49 @@ use Common\Lib\Page;
 use Admin\Model\BaseModel;
 use Admin\Model\RoomModel;
 
-class BoxModel extends BaseModel
-{
+class BoxModel extends BaseModel{
 
-	public function getList($where, $order='id desc', $start=0,$size=5)
-	{	
-		
+	public function getList($where, $order='id desc', $start=0,$size=5){	
 		 $list = $this->where($where)
 					  ->order($order)
 					  ->limit($start,$size)
 					  ->select();
-
-		
 		$count = $this->where($where)
 					  ->count();
-
 		$objPage = new Page($count,$size);		  
-
 		$show = $objPage->admin_page();
-
 		$data = array('list'=>$list,'page'=>$show);
-
         return $data;
-
-
-	}//End Function
-
-
-
-
+	}
 
 	/**
 	 * 包间ID转换为包间名称
 	 * @param  array  $result [description]
 	 * @return [type]         [description]
 	 */
-	public function roomIdToRoomName($result=[])
-	{
-		if(!$result || !is_array($result))
-		{
+	public function roomIdToRoomName($result=[]){
+		if(!$result || !is_array($result)){
 			return [];
 		}
-
 		$arrHotelId = [];
 
-		foreach ($result as $value) 
-		{
+		foreach ($result as $value){
 			$arrHotelId[] = $value['room_id'];
 		}
 
 		$filter       = [];
 		$filter['id'] = ['IN',$arrHotelId];
-
 		$roomModel = new RoomModel;
-		
-		$arrHotel = $roomModel->getAll('id,name',$filter);
-		
-		foreach ($result as &$value) 
-		{
-			foreach ($arrHotel as  $row) 
-			{
-				if($value['room_id'] == $row['id'])
-				{
+		$arrHotel = $roomModel->getAll('id,name',$filter,0,100);
+		foreach ($result as &$value){
+			foreach ($arrHotel as  $row){
+				if($value['room_id'] == $row['id']){
 					$value['room_name'] = $row['name'];
 				}
 			}
 		}
-		
 		return $result;
-
-	}//End Function
-
+	}
 
 
-
-
-}//End Class
+}
