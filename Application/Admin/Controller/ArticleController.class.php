7902f2abacb1f492beead7926c96d1d77e91d5b7
liubin0125 <liubin-0125@163.com>
2017-02-14 23:02:00 +0800

    新增内容时，未添加封面图点击保存提示错误信息

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -30,19 +30,17 @@ class ArticleController extends BaseController {
         $fields = "id, name, oss_addr";
         $where = "1=1";
         $result = $mediaModel->getWhere($where,$fields);
-       return $result;
+        return $result;
     }
 
     public function delart() {
         $gid = I('get.id', 0, 'int');
         //查找是否在首页内容中引用
-
         if($gid) {
             $mbHomeModel = new \Admin\Model\HomeModel();
             $res = $mbHomeModel->where('content_id='.$gid)->find();
             if($res) {
                 $this->output('首页有引用不可删除', 'content/getlist',1);
-                die;
             }
             $artModel = new ArticleModel();
             $result = $artModel -> delData($gid);
@@ -63,12 +61,10 @@ class ArticleController extends BaseController {
         echo $img;
     }
 
-
     /**
      * 添加文章
      */
     public function addAaddrticle(){
-
         $catModel = new CategoModel();
         $artModel = new ArticleModel();
         $userInfo = session('sysUserInfo');
@@ -76,7 +72,6 @@ class ArticleController extends BaseController {
         $this->assign('uname',$uname);
         $id = I('get.id');
         $acctype = I('get.acttype');
-
         $vinfo['state'] = 0;
         $this->assign('vinfo',$vinfo);
         if ($acctype && $id){
@@ -92,6 +87,7 @@ class ArticleController extends BaseController {
         $this->display('addart');
 
     }
+    
     /*
      * 显示并生成H5页面地址
      */
@@ -130,7 +126,6 @@ class ArticleController extends BaseController {
         $replacment = '<img src='.__ROOT__ .'${1}>';
         $url_arr = explode('?id=', $url);
         $url_id = $url_arr['1'];
-        //var_dump($url_id);
         $this->assign('videoaaa', $url_id);
         $this->assign('videobbb', $url_id);
         $content =  preg_replace($pattern, $replacment, $content);
@@ -138,7 +133,6 @@ class ArticleController extends BaseController {
         ob_start();
         $this->display('showvideocontent');
         $content = ob_get_contents();//取得php页面输出的全部内容
-        //echo $content;
         $content = str_replace('videobbbqqqqqqqqqq',$url_id,$content);
         $path = SITE_TP_PATH.'/Public/html/video';
         if ( !(is_dir($path)) ) {
@@ -155,57 +149,42 @@ class ArticleController extends BaseController {
         $mbHomeModel = new \Admin\Model\HomeModel();
         if($id){
             $vinfo = $mbHomeModel->where(array('id'=>$id))->find();
-
             $this->assign('vinfo',$vinfo);
-
         }
         return $this->display('addhome');
     }
 
     public function doaddhome(){
-
         //文章id
         $mbHomeModel = new \Admin\Model\HomeModel();
         $artid = $_REQUEST['artid'];
         $type = $_REQUEST['acttype'];
         $id = I('post.id');
-      //  var_dump($artid,$type);
         if($type == 1){
             //判断表中是否有
-
             $res = $mbHomeModel->where('content_id='.$artid)->find();
-
             if( $res ){
                 $this->output('失败文章已经存在', 'content/getlist',2);
             } else {
-
                 $artModel = new  \Admin\Model\ArticleModel();
                 $arr = $artModel->find($artid);
                 $state = $arr['state'];
                 if ($state != 2) {
                     $this->output('失败审核状态不允许', 'content/getlist',2);
                 }
-
                 //期刊
                 $mbperModel = new \Admin\Model\MbPeriodModel();
                 $num = $mbperModel->count();
-
                 $time = time();
                 $dat['period'] = date("YmdHis",$time);
                 $dat['update_time'] = date("Y-m-d H:i:s",$time);
                 if($num>0){
-                    $sql = "update savor_mb_period set period=".$dat['period'].",
-update_time='".$dat['update_time']."'";
-
+                    $sql = "update savor_mb_period set period=".$dat['period'].",update_time='".$dat['update_time']."'";
                     $rest = $mbperModel->execute($sql);
-
                 }else{
                     $mbperModel->add($dat);
                 }
-
-
                 $mbHomeModel = new \Admin\Model\HomeModel();
-
                 $userInfo = session('sysUserInfo');
                 $save[] = array();
                 $md5 = $arr['vod_md5'];
@@ -217,12 +196,8 @@ update_time='".$dat['update_time']."'";
                 $save['creator_id'] = $userInfo['id'];
                 $save['create_time'] = date("Y-m-d H:i:s", time());
                 $save['update_time'] = $save['create_time'];
-
                 $res = $mbHomeModel->add($save);
                 if($res){
-
-
-
                     $this->output('操作成功!', 'article/homemanager',2);
                 }else{
                     $this->output('操作失败!', 'content/getlist');
@@ -238,18 +213,9 @@ update_time='".$dat['update_time']."'";
             }
 
         }
-
-
-
-
-
-
-
     }
 
     public function homemanager(){
-
-
         $mbHomeModel = new \Admin\Model\HomeModel();
         $artModel = new  \Admin\Model\ArticleModel();
         $catModel = new \Admin\Model\CategoModel;
@@ -265,11 +231,8 @@ update_time='".$dat['update_time']."'";
         $orders = $order.' '.$sort;
         $start  = ( $start-1 ) * $size;
         $where = "1=1";
-
         $result = $mbHomeModel->getList($where,$orders,$start,$size);
-
         $datalist = $artModel->changeIdjName($result['list'], $cat_arr);
-
         $name = I('name');
         if($name){
             //根据id取
@@ -278,9 +241,7 @@ update_time='".$dat['update_time']."'";
         }
         $this->assign('list', $datalist);
         $this->assign('page',  $result['page']);
-
         $this->display('homearticle');
-
     }
 
     /*
@@ -290,7 +251,6 @@ update_time='".$dat['update_time']."'";
         $mbHomeModel = new \Admin\Model\HomeModel();
         $id = I('request.id');
         $flag = I('request.flag');
-
         $save['state'] = $flag;
         if($mbHomeModel->where('id='.$id)->save($save)){
             $message = '更新成功!';
@@ -300,8 +260,6 @@ update_time='".$dat['update_time']."'";
             $url = 'article/homemanager';
         }
         $this->output($message, $url,2);
-
-
     }
 
     /**
@@ -341,7 +299,6 @@ update_time='".$dat['update_time']."'";
     }
 
     public function doAddvideo(){
-        //var_dump($_POST);
         $mediaModel = new \Admin\Model\MediaModel();
         $artModel = new ArticleModel();
         $id                  = I('post.id');
@@ -350,8 +307,6 @@ update_time='".$dat['update_time']."'";
         $save['category_id']        = I('post.cate','','trim');
         $covermedia_id = I('post.covervideo_id','0','intval');//视频封面id
         $media_id = I('post.media_id','0','intval');//视频id
-
-
         $save['source']    = I('post.source','');
         $save['content']    = I('post.content','','htmlspecialchars');
         $save['type']    = 3;
@@ -366,17 +321,11 @@ update_time='".$dat['update_time']."'";
         if($covermedia_id){
             $oss_arr = $mediaModel->find($covermedia_id);
             $oss_addr = $oss_arr['oss_addr'];
-            //$save['oss_addr'] = $oss_addr;
             $save['img_url'] = $oss_addr;
             $save['type'] = 1;
-
-        }else{
-
         }
         if($media_id){
             $oss_arr = $mediaModel->find($media_id);
-            //$save['oss_addr'] = $oss_arr['oss_addr'];
-            //$save['duration'] = $oss_arr['duration'];
             $save['vod_md5'] = $oss_arr['md5'];
             $save['media_id']    = $media_id;
 
@@ -388,7 +337,6 @@ update_time='".$dat['update_time']."'";
                 $save['media_id'] = '';
             }
             $save['content_url'] = 'html/video/'.$id.'.html';
-
             if($artModel->where('id='.$id)->save($save)){
                 $this->showvideocontent($id, $save['tx_url']);
                 $this->output('操作成功!', 'content/getlist');
@@ -397,14 +345,11 @@ update_time='".$dat['update_time']."'";
             }
         }else{
             if(!$covermedia_id) {
-                $this->output('失败封面必填!', 'article/addvideo', 3);
-                die;
+                $this->output('失败封面必填!', 'article/addvideo', 3,0);
             }
-
-
             $ret = $artModel->where(array('title'=>$save['title']))->find();
             if($ret){
-                $this->output('失败文章标题存在!', 'content/getlist',3);
+                $this->output('失败文章标题存在!', 'content/getlist',3,0);
             }
 
             $save['type'] = 3;
@@ -455,14 +400,12 @@ update_time='".$dat['update_time']."'";
     }
 
     public function doAddarticle(){
-
         $artModel = new ArticleModel();
         $id                  = I('post.id');
         $save                = [];
         $save['title']        = I('post.title','','trim');
         $save['category_id']        = I('post.cate','','trim');
         $save['source']    = I('post.source','');
-
         $save['content']    = I('post.content','htmlspecialchars');
         $save['type']    = I('post.ctype','','intval');
         $save['state']    = I('post.state','0','intval');
@@ -470,14 +413,11 @@ update_time='".$dat['update_time']."'";
         $save['bespeak_time'] = I('post.logtime','');
         $save['bespeak'] = 0;
         $mediaid = I('post.media_id');
-
         $mediaModel = new \Admin\Model\MediaModel();
         $oss_addr = $mediaModel->find($mediaid);
         $oss_addr = $oss_addr['oss_addr'];
         $save['img_url'] = $oss_addr;
         $image_host = 'http://'.C('OSS_BUCKET').'.'.C('OSS_HOST').'/';
-       // $oss_addr = $image_host.$oss_addr;
-
         if($id){
             $save['content_url'] = 'html/article/'.$id.'.html';
             if($artModel->where('id='.$id)->save($save)){
@@ -488,13 +428,12 @@ update_time='".$dat['update_time']."'";
             }
         }else{
             if(!$mediaid){
-                $this->output('失败封面必填!', 'content/getlist',3);
+                $this->output('失败封面必填!', 'content/getlist',3,0);
                 die;
             }
             $ret = $artModel->where(array('title'=>$save['title']))->find();
-            //  var_dump($ret);
             if($ret){
-                $this->output('失败文章标题存在!', 'content/getlist',3);
+                $this->output('失败文章标题存在!', 'content/getlist',3,0);
             }
             $save['create_time'] = date('Y-m-d H:i:s');
             $userInfo = session('sysUserInfo');
