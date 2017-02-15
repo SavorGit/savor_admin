11280e28d7ce82e38a7dd59ff1ebb6f523e249ca
liubin0125 <liubin-0125@163.com>
2017-02-15 18:55:37 +0800

    增加客户端访问页面

---
+++ Application/Admin/Controller/ClientController.class.php
@@ -0,0 +1,39 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+ * @desc 客户端页面
+ *
+ */
+class ClientController extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function showcontent(){
+        $id = I('get.id',0,'intval');
+        if($id){
+            $articleModel = new \Admin\Model\ArticleModel();
+            $vinfo = $articleModel->where('id='.$id)->find();
+            $vinfo['content'] = html_entity_decode($vinfo['content']);
+        }else{
+            $vinfo = array();
+        }
+        $this->assign('vinfo',$vinfo);
+        if($vinfo['type']==1){//图文
+            $display_html = 'showcontent';
+        }elseif($vinfo['type']==3){
+            $tx_url = $vinfo['tx_url'];
+            $url_arr = explode('?id=', $tx_url);
+            $url_id = $url_arr['1'];
+            $play_js = "(function(){ var option ={'auto_play':'0','file_id':'$url_id','app_id':'1252891964','width':1280,'height':720,'https':1};new qcVideo.Player('id_video_container_$url_id', option ); })()";
+            $this->assign('videourl_id', $url_id);
+            $this->assign('play_js', $play_js);
+            $display_html = 'showvideocontent';
+        }else{
+            $display_html = 'showcontent';
+        }
+        $this->display($display_html);
+    }
+}
