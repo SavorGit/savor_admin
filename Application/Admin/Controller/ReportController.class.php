b108bf294fe42476dc72bc53474210f725502a23
xiaobaig0407 <xiaobaig0407@163.com>
2017-02-20 16:54:35 +0800

    添加心跳显示

---
+++ Application/Admin/Controller/ReportController.class.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ *@author hongwei
+ * @desc 心跳显示列表
+ */
+namespace Admin\Controller;
+
+use Admin\Controller\BaseController;
+
+class ReportController extends BaseController{
+
+	public $path = 'category/img';
+	public $oss_host = '';
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+	/**
+	 * 分类列表
+	 * @return [type] [description]
+	 */
+	public function heart(){
+
+		$heartModel = new \Admin\Model\HeartLogModel();
+		$size   = I('numPerPage',50);//显示每页记录数
+		$this->assign('numPerPage',$size);
+		$start = I('pageNum',1);
+		$this->assign('pageNum',$start);
+		$order = I('_order','last_heart_time');
+		$this->assign('_order',$order);
+		$sort = I('_sort','desc');
+		$this->assign('_sort',$sort);
+		$orders = $order.' '.$sort;
+		$start  = ( $start-1 ) * $size;
+		$where = "1=1";
+		$name = I('name');
+		if($name){
+			$this->assign('name',$name);
+			$where .= "	AND hotel_name LIKE '%{$name}%'";
+		}
+		$result = $heartModel->getList($where,$orders,$start,$size);
+		$time = time();
+		foreach ($result['list'] as &$val) {
+			$d_time = strtotime($val['last_heart_time']);
+			$diff = $time - $d_time;
+			if($diff< 3600) {
+				$val['last_heart_time'] = floor($diff/60).'分';
+
+			}else if ($diff >= 3600 && $diff <= 86400) {
+				$hour = floor($diff/3600);
+				$min = floor($diff%3600/60);
+				$val['last_heart_time'] = $hour.'小时'.$min.'分';
+			}else if ($diff > 86400) {
+				$day = floor($diff/86400);
+				$hour = floor($diff%86400/3600);
+				$val['last_heart_time'] = $day.'天'.$hour.'小时';
+			}
+		}
+
+		$this->assign('list', $result['list']);
+		$this->assign('page',  $result['page']);
+		$this->display('heartlist');
+	}
+
+
+	/**
+	 * 新增分类
+	 *
+	 */
+	public function addCate(){
+		$id = I('get.id');
+		$catModel = new CategoModel;
+		if($id){
+			$vinfo = $catModel->find($id);
+			$image_host = 'http://'.C('OSS_BUCKET').'.'.C('OSS_HOST').'/';
+			$vinfo['oss_addr'] = $image_host.$vinfo['img_url'];
+			$this->assign('vinfo',$vinfo);
+		}
+		return $this->display('addCat');
+	}
+
+
+	/*
+	 * 修改状态
+	 */
+
+	public function changestate(){
+		$cid = I('post.cid');
+		$save = array();
+		$save['state'] = I('post.state');
+		$catModel = new CategoModel;
+		$res_save = $catModel->where('id='.$cid)->save($save);
+		if($res_save){
+			echo 1;
+		} else {
+			echo 0;
+		}
+	}
+
+	/**
+	 * 保存或者更新分类信息
+	 * @return [type] [description]
+	 */
+	public function doAddCat(){
+		$catModel = new CategoModel;
+		$id                  = I('post.id');
+		$save                = [];
+		$save['name']        = I('post.cat_name','','trim');
+		$save['sort_num']    = I('post.sort','','intval');
+
+		$save['update_time'] = date('Y-m-d H:i:s');
+		$mediaid = I('post.media_id');
+		$mediaModel = new \Admin\Model\MediaModel();
+		//$mediaid = 11;
+		$oss_addr = $mediaModel->find($mediaid);
+		$oss_addr = $oss_addr['oss_addr'];
+		$save['img_url'] = $oss_addr;
+		if($id){
+			$res_save = $catModel->where('id='.$id)->save($save);
+			if($res_save){
+				$this->output('操作成功!', 'release/category');
+			}else{
+				$this->output('操作失败!', 'release/doAddCat');
+			}
+		}else{
+			$save['state']    =  0;
+			$save['create_time'] = date('Y-m-d H:i:s');
+			//刷新页面，关闭当前
+			$res_save = $catModel->add($save);
+			if($res_save){
+			    $this->output('添加分类成功!', 'release/category');
+			}else{
+				$this->output('操作失败!', 'release/doAddCat');
+			}
+		}
+	}
+
+}
