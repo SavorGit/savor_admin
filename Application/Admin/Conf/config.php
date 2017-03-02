47cc39fef57be2c84fb14b3e354e1282f701e5c9
xiaobaig0407 <xiaobaig0407@163.com>
2017-03-02 14:39:53 +0800

    修改文章发布路由规则

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,7 +1,13 @@
 <?php
 //系统配置
+$route_rules = array(
+    '/^content\/(\d{0,10})$/'=>'Client/showcontent?id=:1',
+);
 $config = array(
     'VAR_PAGE'=>'pageNum',
     'SHOW_PAGE_TRACE'=>false,
+
+    'URL_ROUTER_ON'   => true,
+    'URL_ROUTE_RULES'=>$route_rules
 );
 return $config;
