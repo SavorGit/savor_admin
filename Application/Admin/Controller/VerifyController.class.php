85869bd34dcd813a021dd09abfafe7440e1a4a0c
zhangyingtao75144 <125929019@qq.com>
2017-03-09 16:39:38 +0800

    修改验证码样式

--- Application/Admin/Controller/VerifyController.class.php
+++ Application/Admin/Controller/VerifyController.class.php
@@ -13,7 +13,12 @@ class VerifyController extends Controller {
     {
             $config = [
                'fontSize' => 16, // 验证码字体大小
+                'useCurve'  =>  false,
+                'useNoise'  =>  false,
                 'length' => 4, // 验证码位数
+                'imageH'    =>  0,               // 验证码图片高度
+                'imageW'    =>  0,               // 验证码图片宽度
+                'bg'        =>  array(243, 251, 254),  // 背景颜色
                  'imageH' => 30
                ];
          $Verify = new Verify($config);
