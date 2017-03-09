e0e808b9bc1b6b3d084a8de5bf39fdaae6167b96
zhangyingtao75144 <125929019@qq.com>
2017-03-09 09:55:45 +0800

    修改倒表方法

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -1,15 +1,14 @@
 <?php
-namespace Think\Controller;
+namespace Admin\Controller;
+use Think\Controller;
 // use Common\Lib\SavorRedis;
 /**
  * @desc 功能测试类
  *
  */
 class TestController extends Controller {
-    
-    public function __construct() {
-       // parent::__construct();
-    }
+                             
+   
 
     public function getarsize(){
         exit();
@@ -44,41 +43,58 @@ class TestController extends Controller {
 
     }
 
-    public function exportExcel($expTitle,$expCellName,$expTableData){
-
-        vendor("PHPExcel.PHPExcel.IOFactory");
-        vendor("PHPExcel.PHPExcel");
-        $filetmpname = APP_PATH.'../public/2.xls';
-        $objPHPExcel = new \PHPExcel();
+public function exportExcel($expTitle,$expCellName,$expTableData){
+    vendor("PHPExcel.PHPExcel.IOFactory");
+    vendor("PHPExcel.PHPExcel");
         $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称
-        $fileName = date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
+        $fileName = $_SESSION['account'].date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
         $cellNum = count($expCellName);
         $dataNum = count($expTableData);
+        vendor("PHPExcel.PHPExcel");
+       
+        $objPHPExcel = new \PHPExcel();
         $cellName = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
-
+        
         $objPHPExcel->getActiveSheet(0)->mergeCells('A1:'.$cellName[$cellNum-1].'1');//合并单元格
-        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle.'  Export time:'.date('Y-m-d H:i:s'));
+       // $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle.'  Export time:'.date('Y-m-d H:i:s'));  
         for($i=0;$i<$cellNum;$i++){
-            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i].'2', $expCellName[$i][1]);
-        }
-        // Miscellaneous glyphs, UTF-8
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i].'2', $expCellName[$i][1]); 
+        } 
+          // Miscellaneous glyphs, UTF-8   
         for($i=0;$i<$dataNum;$i++){
-            for($j=0;$j<$cellNum;$j++){
-                $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j].($i+3), $expTableData[$i][$expCellName[$j][0]]);
-            }
-        }
-
-
-        ob_end_clean();
-        header('Content-Type: application/vnd.ms-excel');
+          for($j=0;$j<$cellNum;$j++){
+            $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j].($i+3), $expTableData[$i][$expCellName[$j][0]]);
+          }             
+        }  
+        
+        header('pragma:public');
+        header('Content-type:application/vnd.ms-excel;charset=utf-8;name="'.$xlsTitle.'.xls"');
         header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印
-        header('Cache-Control: max-age=0');
-
-        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-
-        $objWriter->save('php://output');
-
-        exit;
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');  
+        $objWriter->save('php://output'); 
+        exit;   
+    }
+/**
+     *
+     * 导出Excel
+     */
+    function expUser(){//导出Excel
+        $xlsName  = "User";
+        $xlsCell  = array(
+        array('id','账号序列'),
+        array('username','名字'),
+        array('tel','电话'),
+         
+        );
+        $xlsModel = M('a1');
+    
+        $xlsData  = $xlsModel->Field('id,username,tel')->select();
+        foreach ($xlsData as $k => $v)
+        {
+            $xlsData[$k]['sex']=$v['sex']==1?'男':'女';
+        }
+        $this->exportExcel($xlsName,$xlsCell,$xlsData);
+         
     }
 
     public function exportExcelbake($expTitle,$expCellName,$expTableData){
