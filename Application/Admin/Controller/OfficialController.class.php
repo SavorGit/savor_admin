61f652700c633669bd6b0691baf9dfb7dcf6d60c
zhangyingtao75144 <125929019@qq.com>
2017-03-24 15:36:29 +0800

    增加地图分页接口

--- Application/Admin/Controller/OfficialController.class.php
+++ Application/Admin/Controller/OfficialController.class.php
@@ -31,7 +31,30 @@ class officialController extends Controller {
 	    //echo  json_encode($result);exit;
 	   echo  "login(".json_encode($result).")";
 	}
-	
+	public function getHotelByPage(){
+	    $pageSize = I('get.pageSize','12','intval');   //每页条数
+	    $pageNo  = I('get.pageNo','1','intval');       //当前页数
+	    $offset = ($pageNo-1) * $pageSize;
+	    $hotelMode = new \Admin\Model\HotelModel();
+	    $result =  array();
+	    $map['state'] = 1;
+	    $limit = "$offset,$pageSize";
+	    $list = $hotelMode->getInfo('id,name,gps,addr',$map,'',$limit);
+	    foreach($list as $keyd=>$v){
+	       $tmp = array();
+	        if(!empty($v['gps'])){
+	           $gps_arr = explode(',', $v['gps']);
+	           $tmp['id'] = $v['id'];
+	           $tmp['name'] = $v['name'];
+	           $tmp['gps'] = $v['gps'];
+	           $tmp['lng'] = $gps_arr[0];
+	           $tmp['lat'] = $gps_arr[1];
+	           $tmp['addr'] = $v['addr'];
+	           $result[] = $tmp;
+	       }
+	    }
+	    echo "hotel(".json_encode($result).")";
+	}
 }
 
 ?>
