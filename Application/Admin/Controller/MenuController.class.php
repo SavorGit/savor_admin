b72fcd96b8d813273d6de154bc4af789c9526a58
xiaobaig0407 <xiaobaig0407@163.com>
2017-03-24 14:26:49 +0800

    修改节目单日志bug

--- Application/Admin/Controller/MenuController.class.php
+++ Application/Admin/Controller/MenuController.class.php
@@ -524,6 +524,9 @@ class MenuController extends BaseController {
     /*
      * 获取log日志，并进行对比
      */
+
+
+
     public function getlog() {
         $userInfo = session('sysUserInfo');
         $menu_id = I('id');
@@ -563,6 +566,7 @@ class MenuController extends BaseController {
                 }
                 //新的没有旧的有
                 $acn = array_diff($bak, $sec);
+                $acn = array_unique($acn);
                 $sec = array_count_values($sec);
                 $bak = array_count_values($bak);
                 $arr_add = array();
@@ -619,7 +623,6 @@ class MenuController extends BaseController {
         $this->assign('vinfo', $data);
         $this->display('opelog');
     }
-
     public function getHotelInfo(){
         $befo  = C('DB_PREFIX');
         $menu_id = I('menuid');
