081bc603cbc01b7d7f7f4de373232a36eae58a94
zhangyingtao75144 <125929019@qq.com>
2017-03-28 16:31:55 +0800

    修改地图酒楼是否可以投屏

--- Application/Admin/Controller/OfficialController.class.php
+++ Application/Admin/Controller/OfficialController.class.php
@@ -14,7 +14,7 @@ class officialController extends Controller {
 	    $map = array();
 	    $map['state'] =1;
 	    
-	    $list = $HotelModel->getInfo('id,name,gps,addr',$map);
+	    $list = $HotelModel->getInfo('id,name,gps,addr,hotel_box_type',$map);
 	    foreach($list as $keyd=>$v){
 	       $tmp = array();
 	        if(!empty($v['gps'])){
@@ -25,6 +25,15 @@ class officialController extends Controller {
 	           $tmp['lng'] = $gps_arr[0];
 	           $tmp['lat'] = $gps_arr[1];
 	           $tmp['addr'] = $v['addr'];
+	           if(empty($v['hotel_box_type'])){
+	               $tmp['is_screen'] = 0;
+	           }else {
+	               if($v['hotel_box_type'] ==1 || $v['hotel_box_type'] ==2){
+	                   $tmp['is_screen'] = 0;
+	               }else if($v['hotel_box_type'] ==3){
+	                   $tmp['is_screen'] = 1;
+	               }
+	           }
 	           $result[] = $tmp;
 	       }
 	    }
