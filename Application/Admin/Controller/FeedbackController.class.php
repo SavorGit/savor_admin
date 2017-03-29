4bdcf2b46f39db625eafe5c6bc06fd0eb4b88e48
xiaobaig0407 <xiaobaig0407@163.com>
2017-03-29 17:32:59 +0800

    添加意见反馈功能

---
+++ Application/Admin/Controller/FeedbackController.class.php
@@ -0,0 +1,127 @@
+<?php
+namespace Admin\Controller;
+/**
+ * @desc 功能测试类
+ *
+ */
+use Admin\Controller\BaseController;
+use Admin\Model\HomeModel;
+class FeedbackController extends BaseController {
+    var $content_type_arr;
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getlist(){
+        $artModel = new ArticleModel();
+        $homeModel = new HomeModel();
+        $size   = I('numPerPage',50);//显示每页记录数
+        $this->assign('numPerPage',$size);
+        $start = I('pageNum',1);
+        $this->assign('pageNum',$start);
+        $order = I('_order','update_time');
+        $this->assign('_order',$order);
+        $sort = I('_sort','desc');
+        $this->assign('_sort',$sort);
+        $orders = $order.' '.$sort;
+        $start  = ( $start-1 ) * $size;
+
+        $where = "1=1";
+        $name = I('titlename');
+        $type = I('type',10,'intval');//10为全部
+        //$where .= " AND state=2 ";
+        $category_id = I('category_id',0,'intval');
+        if($category_id) $where .=" AND category_id='$category_id'";
+        $content_type = I('content_type','');
+        
+        if(is_numeric($content_type)){
+            switch ($content_type){
+                case 0:
+                    $where .= " AND type=0";
+                    break;
+                case 1:
+                    $where .=" AND type=1";
+                    break;
+                case 2:
+                    $where .=" AND type=2";
+                    break;
+                case 3:
+                    $where .=" AND type=3 and media_id=0";
+                    break;
+                case 4:
+                    $where .=" AND type=3 and media_id>0"; 
+                    break;
+            }
+        }
+        $this->assign('content_type',$content_type);
+        $beg_time = I('begin_time','');
+        $end_time = I('end_time','');
+        if($beg_time)   $where.=" AND create_time>='$beg_time'";
+        if($end_time)   $where.=" AND create_time<='$end_time 23:59:59'";
+        if($name){
+            $this->assign('name',$name);
+            $where .= "	AND title LIKE '%{$name}%'";
+        }
+        if($type!=10){
+            $where .= "	AND type='$type'";
+        }
+        $result = $artModel->getList($where,$orders,$start,$size);
+	    $result['list'] = $artModel->changeCatname($result['list']);
+	    $result['list'] = $homeModel->ishomeContent($result['list']);
+	    $catModel = new \Admin\Model\CategoModel();
+	    $where = " state=1";
+    	$field = 'id,name';
+    	$category_list = $catModel->getWhere($where, $field);
+    	$this->assign('vcainfo',$category_list);
+	    $time_info = array('now_time'=>date('Y-m-d H:i:s'),'begin_time'=>$beg_time,'end_time'=>$end_time);
+        $this->assign('content_type_arr',$this->content_type_arr);
+	    $this->assign('category_id',$category_id);
+	    $this->assign('timeinfo',$time_info);
+        $this->assign('ctype', $type);
+        $this->assign('list', $result['list']);
+        $this->assign('page',  $result['page']);
+        $this->display('content');
+    }
+
+
+
+    public function rplist(){
+        $fdModel = new \Admin\Model\FeedbackModel();
+        $size   = I('numPerPage',50);//显示每页记录数
+        $this->assign('numPerPage',$size);
+        $start = I('pageNum',1);
+        $this->assign('pageNum',$start);
+        $order = I('_order','create_time');
+        $this->assign('_order',$order);
+        $sort = I('_sort','desc');
+        $this->assign('_sort',$sort);
+        $orders = $order.' '.$sort;
+        $start  = ( $start-1 ) * $size;
+        $where = "1=1";
+        $name = I('titlename');
+        $beg_time = I('begin_time','');
+        $end_time = I('end_time','');
+        $d_arr = C('DEVICE_TYPE');
+        if($beg_time)   $where.=" AND create_time>='$beg_time'";
+        if($end_time)   $where.=" AND create_time<='$end_time'";
+        if($name){
+            $this->assign('name',$name);
+            $where .= "	AND suggestion LIKE '%{$name}%'";
+        }
+        $result = $fdModel->getList($where,$orders,$start,$size);
+        $res = $result['list'];
+        foreach($res as $rk=>$rv){
+            foreach($d_arr as $k=>$v){
+                if($rv['device_type'] == $k) {
+                    $res[$rk]['device_type'] = $v;
+                }
+            }
+        }
+        $time_info = array('now_time'=>date('Y-m-d H:i:s'),'begin_time'=>$beg_time,'end_time'=>$end_time);
+        $this->assign('timeinfo',$time_info);
+        $this->assign('list', $res);
+        $this->assign('page',  $result['page']);
+        $this->display('feedlist');
+    }
+
+}
