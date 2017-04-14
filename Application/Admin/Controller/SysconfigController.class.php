634cf5634b07c252f39a246d968a1ef37320b32e
zhangyingtao75144 <125929019@qq.com>
2017-04-14 18:25:27 +0800

    后台设置音量

--- Application/Admin/Controller/SysconfigController.class.php
+++ Application/Admin/Controller/SysconfigController.class.php
@@ -17,11 +17,20 @@ class SysconfigController extends BaseController {
      */
     public function configData(){
         $m_sys_config = new \Admin\Model\SysConfigModel();
-        $volume_info = $m_sys_config->getOne('system_default_volume');
+        //$volume_info = $m_sys_config->getOne('system_default_volume');
         $switch_time_info = $m_sys_config->getOne('system_switch_time');
-        $info['system_default_volume'] = $volume_info['config_value'];
+        
+        $condition['config_key'] = 'system_default_volume';
+        $condition['config_key'] = 'system_switch_time';
+        $condition['config_key'] = 'system_demand_video_volume';
+        $condition['config_key'] = 'system_tv_volume';
+        $condition['_logic'] = 'OR';
+        
+        $volume_arr = $m_sys_config->getList($condition);
+        
+        //$info['system_default_volume'] = $volume_info['config_value'];
         $info['system_switch_time']  = $switch_time_info['config_value'];
-        $info['status'] = $volume_info['status'];
+        $info['status'] = $switch_time_info['status'];
         
         //视频投屏loading图
         $loading_info = $m_sys_config->getOne('system_loading_image');
@@ -42,31 +51,31 @@ class SysconfigController extends BaseController {
      * @desc 修改设置
      */
     public function doConfigData(){
-        $system_default_volume = I('post.system_default_volume',0,'intval');
+        //$system_default_volume = I('post.system_default_volume',0,'intval');
         $system_switch_time = I('post.system_switch_time',0,'intval');
         $m_sys_config = new \Admin\Model\SysConfigModel();
-        $volume_info = $m_sys_config->getOne('system_default_volume'); 
+        //$volume_info = $m_sys_config->getOne('system_default_volume'); 
   
-        if(empty($volume_info)){
+        /* if(empty($volume_info)){
             $data['config_key'] = 'system_default_volume';
             $data['config_value'] = $system_default_volume;;
             $rt = $m_sys_config->add($data);
         }else {
             $data['config_value'] = $system_default_volume;
             $rt = $m_sys_config->editData($data, 'system_default_volume');
-        }
+        } */
  
-            $switch_time_info = $m_sys_config->getOne('system_switch_time');
-            if(empty($switch_time_info)){
-                $map['config_key'] = 'system_switch_time';
-                $map['config_value'] = $system_switch_time;
-                $rts = $m_sys_config->add($map);
-            }else {
-                $map['config_value'] = $system_switch_time;
-                $rts = $m_sys_config->editData($map, 'system_switch_time',2);
-            }
+        $switch_time_info = $m_sys_config->getOne('system_switch_time');
+        if(empty($switch_time_info)){
+            $map['config_key'] = 'system_switch_time';
+            $map['config_value'] = $system_switch_time;
+            $rts = $m_sys_config->add($map);
+        }else {
+            $map['config_value'] = $system_switch_time;
+            $rts = $m_sys_config->editData($map, 'system_switch_time',2);
+        }
         
-        if($rts || $rt){
+        if($rts){
             $this->output('操作成功','sysconfig/configData');
         }else {
             $this->error('操作失败');
@@ -90,10 +99,31 @@ class SysconfigController extends BaseController {
             $this->output('操作成功!', 'sysconfig/configData',2);
         }else {
             $tranDb->rollback();
-            $this->error('操作失败3!');
+            $this->error('操作失败!');
         }
         
     }
+    /**
+     * @desc 音量设置
+     */
+    public function doConfigVolume(){
+        $data = array();
+        $data['system_ad_volume'] = I('post.system_ad_volume',0,'intval');             //广告轮播音量
+        
+        $data['system_pro_screen_volume']   = I('post.system_pro_screen_volume',0,'intval');    //投屏音量
+        $data['system_demand_video_volume'] = I('post.system_demand_video_volume',0,'intval');   //点播音量
+        $data['system_tv_volume']           = I('post.system_tv_volume',0,'intval');             //电视音量
+        
+        $m_sys_config = new \Admin\Model\SysConfigModel();
+        $ret = $m_sys_config->updateInfo($data);
+        
+        if($ret){
+            $this->output('操作成功!', 'sysconfig/configData',2);
+        }else {
+            $this->error('操作失败3!');
+        }
+    }
+    
     /**
      * @desc 修改视频投屏loading图
      */
