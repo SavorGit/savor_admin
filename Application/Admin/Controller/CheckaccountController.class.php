0b43c133830743065962e52cdc3fe16fa9724a65
xiaobaig0407 <xiaobaig0407@163.com>
2017-06-30 20:29:22 +0800

    添加脚本定时文件

--- Application/Admin/Controller/CheckaccountController.class.php
+++ Application/Admin/Controller/CheckaccountController.class.php
@@ -527,14 +527,17 @@ class CheckaccountController extends BaseController{
 			}else{
 				//判断id是否存在hotel表
 				$dat['id'] = $rv['id'];
+
 				//$field = 'id,name,bill_per,bill_tel';
 				$finfo = $hotelModel->find($dat['id']);
-
+			//	var_dump( $finfo);
 				if($finfo){
 					$info[$rk]['bill_tel'] = $finfo['bill_tel'];
 					$num[] = $rv['id'];
-					$info[$rk]['state'] = 2;
 					$where = ' 1=1 and state=1 and cost_type='.$fee;
+					//判断酒楼是否下发
+					//ft<=en   开始值要小于给出结束值
+					//fe>=st   结束值要大于给出开头值
 					if($st){
 						$where .= " and fee_end >='".$st."'";
 					}
@@ -542,17 +545,18 @@ class CheckaccountController extends BaseController{
 						//$start_date = date('YmdH',strtotime($start_date));
 						$where .= " AND fee_start <='".$en."' AND hotel_id = ".$rv['id'];
 					}
+					$field = '`hotel_id`';
 					$rest = $statedetailModel->getWhereData($where,$field);
 					if($rest){
 						$info[$rk]['state'] = 3;
 						$info[$rk]['name'] = $finfo['name'];
 						continue;
 					}else{
-						if(empty($res[$rk]['money']) ||  (!is_numeric($res[$rk]['money']))){
+						if(empty($info[$rk]['money']) ||  (!is_numeric($info[$rk]['money']))){
 							$info[$rk]['state'] = 7;
 							continue;
 						}else{
-							if($res[$rk]['money']<0){
+							if($info[$rk]['money']<0){
 								$info[$rk]['state'] = 5;
 								continue;
 							}
@@ -570,216 +574,10 @@ class CheckaccountController extends BaseController{
 		}
 		return $info;
 		die;
-		foreach($info as $rk=>$rv) {
-			//$info[$rk]['sort_num'] = $rk;
-			if(in_array($rv['id'], $num)){
-				$repeat_arr[$rk] = $rv;
-				continue;
-			}else if(empty($rv['id'])){
-				$emparray[$rk] = $rv;
-				continue;
-			} else if(!is_int($rv['id'])){
-				$ill_hotel[$rk] = $rv;
-				continue;
-			}else{
-				$num[] = $rv['id'];
-				$money[$rv['id']] = $rv['money'];
-				$sort[$rv['id']] = $rk;
-				$rest[$rv['id']] = $rv;
-				continue;
-			}
-		}
-
-		$num_str = implode(',', $num);
-		$dat['id']=array('in',$num_str);
-		$dat['flag']= 0;
-		$field = 'id,name,bill_per,bill_tel';
-		$res = $hotelModel->getWhereData($dat, $field);
-		$num_true = array();
-		foreach($res as $rk=>$rv) {
-			$res[$rk]['money'] = $money[$rv['id']];
-			$num_true[] = $rv['id'];
-			if($res[$rk]['money']<0){
-				$res[$rk]['state'] = 5;
-				continue;
-			}
-			if(!is_numeric($res[$rk]['money'])){
-				$res[$rk]['state'] = 7;
-				continue;
-			}
-
-
-		}
-		$count = count($num_true);
-		$ar_diff = array_diff($num, $num_true);
-		//找到状态为2即不存在
-		foreach($ar_diff as $av){
-			$res[$count]['id'] = $av;
-			$res[$count]['state'] = 2;
-			$res[$count]['name'] = $rest[$av]['name'];
-			$res[$count]['money'] = $rest[$av]['money'];
-			$res[$count]['bill_per'] = '';
-			$res[$count]['bill_tel'] = '';
-			$count++;
-		}
-
-		if($repeat_arr){
-			foreach($repeat_arr as $rk=>$rv){
-				$res[$count]['id'] = $rv['id'];
-				$res[$count]['state'] = 6;
-				$res[$count]['name'] = $rv['name'];
-				$res[$count]['money'] = $rv['money'];
-				$res[$count]['bill_per'] = '';
-				$res[$count]['bill_tel'] = '';
-				$count++;
-			}
-		}
-
-		if($ill_hotel){
-			foreach($ill_hotel as $rk=>$rv){
-				$res[$count]['id'] = $rv['id'];
-				$res[$count]['state'] = 2;
-				$res[$count]['name'] = $rv['name'];
-				$res[$count]['money'] = $rv['money'];
-				$res[$count]['bill_per'] = '';
-				$res[$count]['bill_tel'] = '';
-				$count++;
-			}
-		}
-		if($emparray){
-			foreach($emparray as $rk=>$rv){
-				$res[$count]['id'] = $rv['id'];
-				$res[$count]['state'] = 2;
-				$res[$count]['name'] = $rv['name'];
-				$res[$count]['money'] = $rv['money'];
-				$res[$count]['bill_per'] = '';
-				$res[$count]['bill_tel'] = '';
-				$count++;
-			}
-		}
-
-
-
-		//判断酒楼是否下发
-		//ft<=en   开始值要小于给出结束值
-        //fe>=st   结束值要大于给出开头值
-		$where = ' 1=1 and state=1 and cost_type='.$fee;
-		if($st){
-			$where .= " and fee_end >='".$st."'";
-		}
-		if($en){
-			//$start_date = date('YmdH',strtotime($start_date));
-			$where .= " AND fee_start <='".$en."' ";
-		}
-		if($num_true){
-			$num_true_str = implode(',', $num_true);
-			$where .= "and hotel_id in ($num_true_str)";
-		}
-		$field = '`hotel_id`';
-		$rest = $statedetailModel->getWhereData($where,$field);
-		foreach($rest as $rv){
-			$numpp = $rv['hotel_id'];
-			$fee_time_num[$numpp] = $numpp;
-		}
-		foreach($res as $rk=>$rv) {
-
-			if ( isset($fee_time_num[$rv['id']]) &&  !isset($res[$rk]['state'])){
-
-				$res[$rk]['state'] = 3;
-
-			}else{
-				if(empty($res[$rk]['bill_tel']) && empty($res[$rk]['state'])){
-					$res[$rk]['state'] = 4;
-				}
-			}
-		}
-		return $res;
 	}
 
 
-	/*
-	 * 执行脚本文件定时发送短信
-	 */
-	public function sendToSeller(){
 
-		//http://www.a.com/index.php/checkaccount/sendToSeller
-		//http://devp.admin.rerdian.com/index.php/checkaccount/sendToSeller
-		$redis  =  SavorRedis::getInstance();
-		$redis->select(15);
-		$rkey = 'savor_account_statement_notice';
-		$roll_back_arr = array();
-		$suca = array();
-		$count = 0;
-		$maxcount = 8;
-		$max = $redis->lsize($rkey);
-		$data = $redis->lgetrange($rkey,0,$max);
-		var_dump($data);
-		$statedetailModel = new \Admin\Model\AccountStatementDetailModel();
-		$statenoticeModel = new \Admin\Model\AccountStatementNoticeModel();
-		$me_su_arr = array();
-		$me_fail_arr = array();
-		if(empty($data)){
-			echo '数组为空'."\n";
-			die;
-		}
-		//http://www.a.com/showMesLink/1be79d87c7f8360f
-		foreach ($data as $val){
-			//获取短信发送最大值数量
-			$field = 'count,id noticeid, f_type ftype';
-			$dat['detail_id'] = $val;
-			$notice_arr = $statenoticeModel->getWhere($dat, $field);
-			if($notice_arr){
-				$count = $notice_arr['count'];
-				$noticeid = $notice_arr['noticeid'];
-				$redis->lPop($rkey);
-				if ($count >= 8 ) {
-					continue;
-				} else {
-					//发送短信
-					$info = $statedetailModel->getWhereSql($val);
-					$m_state = $this->sendMessage($info);
-					var_dump($m_state);
-					if($m_state){
-						$me_su_arr[] = $noticeid;
-						continue;
-					}else{
-						$roll_back_arr[] = $val;
-						$me_fail_arr[] = $noticeid;
-						continue;
-					}
-
-				}
-			}else{
-				echo '出错ID:'.$val.'<br/>';
-				$redis->lPop($rkey);
-			}
-
-		}
-		if($roll_back_arr){
-			//更新count字段
-			foreach($roll_back_arr as $k){
-				$redis->rPush($rkey, $k);
-			}
-		}
-		if($me_su_arr){
-			$me_su_str = 'values';
-			$where = 'status = 1';
-			foreach($me_su_arr as $ma){
-				$me_su_str .=  ' ('.$ma.')'.',';
-			}
-			$me_su_str = substr($me_su_str,0,-1);
-			$statenoticeModel->insertDup($me_su_str, $where);
-		}
-		if($me_fail_arr){
-			$me_fail_str = 'values';
-			$where = '`count` = `count` + 1';
-			foreach($me_fail_arr as $ma){
-				$me_fail_str .=  ' ('.$ma.')'.',';
-			}
-			$me_fail_str = substr($me_fail_str,0,-1);
-			$statenoticeModel->insertDup($me_fail_str, $where);
-		}
-	}
 
 
 	private function addAccountLog($sjson,$param,$to){
