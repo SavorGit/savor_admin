dc106d953ec71dd28f487111e3a363b05ebca78d
xiaobaig0407 <xiaobaig0407@163.com>
2017-07-05 15:25:36 +0800

    修改详情样式及添加例外方法
    1.MOD 方法

--- Application/Admin/Controller/BaseController.class.php
+++ Application/Admin/Controller/BaseController.class.php
@@ -497,7 +497,7 @@ class BaseController extends Controller {
         $free_action = array('admin.menu.get_se_left','admin.clean.cache','admin.resource.uploadresource',
                              'admin.resource.uploadresourcenew','admin.user.chagepwd','admin.boxaward.getaward',
                              'admin.hotel.manager_list','admin.version.getvname','admin.menu.getfile',
-                             'admin.menu.analyseexcel','admin.checkaccount.getaccountinfo','admin.checkaccount.analyseExcel'
+                             'admin.menu.analyseexcel','admin.checkaccount.getaccountinfo','admin.checkaccount.analyseexcel'
         );
         $model_name      = strtolower(MODULE_NAME);
         $controller_name = strtolower(CONTROLLER_NAME);
