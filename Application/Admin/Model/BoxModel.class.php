1323fea5a74e6da78716d5680d45af235caeb61d
xiaobaig0407 <xiaobaig0407@163.com>
2017-07-26 15:11:04 +0800

    修改导出资源表字段为电视状态

--- Application/Admin/Model/BoxModel.class.php
+++ Application/Admin/Model/BoxModel.class.php
@@ -22,7 +22,7 @@ class BoxModel extends BaseModel{
 		}
 		$t_str = substr($t_str,0,-1);
 		$Model = new \Think\Model();
-	 $sql = 'select hotel.id,hotel.install_date, hotel.state hsta, room.state rsta, box.mac mac, room.name rname, room.type rtype, tv.tv_brand tbrd, tv.tv_size tsiz, tv.tv_source, hotel.name hname, hotel.level, hotel.area_id, hotel.addr, hotel.contractor, hotel.mobile, hotel.tel, hotel.iskey, hotel.maintainer, hotel.tech_maintainer from savor_tv as tv left join savor_box as box on tv.box_id = box.id left join savor_room as room on box.room_id = room.id left join savor_hotel as hotel on room.hotel_id = hotel.id where tv.id in ('.$t_str.')';
+	 $sql = 'select hotel.id,hotel.install_date, hotel.state hsta, room.state rsta,tv.state tsta, box.mac mac, room.name rname, room.type rtype, tv.tv_brand tbrd, tv.tv_size tsiz, tv.tv_source, hotel.name hname, hotel.level, hotel.area_id, hotel.addr, hotel.contractor, hotel.mobile, hotel.tel, hotel.iskey, hotel.maintainer, hotel.tech_maintainer from savor_tv as tv left join savor_box as box on tv.box_id = box.id left join savor_room as room on box.room_id = room.id left join savor_hotel as hotel on room.hotel_id = hotel.id where tv.id in ('.$t_str.')';
 		$volist = $Model->query($sql);
 
 	 $res = $this->changeInfoName($volist);
@@ -64,6 +64,9 @@ class BoxModel extends BaseModel{
 				}
 				if($value['rsta'] == $k){
 					$value['rsta'] = $v;
+				}
+				if($value['tsta'] == $k){
+					$value['tsta'] = $v;
 				}
 			}
 			foreach ($r_arr as  $k=>$v){
