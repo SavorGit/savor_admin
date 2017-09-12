74708de11c10b5dbfa3edb4eb9273cd7b7590531
zhangyingtao75144 <125929019@qq.com>
2017-09-12 14:45:19 +0800

    手机验证码错误和已过去做区分

--- Application/Admin/Controller/ActivitydetailController.class.php
+++ Application/Admin/Controller/ActivitydetailController.class.php
@@ -827,14 +827,34 @@ class ActivitydetailController extends Controller {
             $cache_verify_code = $redis->get($cache_key);
             
             if(empty($cache_verify_code)){
-                $map['status'] = 107;
-                $map['extent'] = 110;
-                $map['msg']    = '手机验证码已过期';
-                echo json_encode($map);
-                exit;
+                
+                $m_account_msg_log = new \Admin\Model\AccountMsgLogModel();
+                
+                $info = $m_account_msg_log->getOne(array('tel'=>$mobile,'msg_type'=>2,'resp_code'=>'000000'),' id desc');
+                if(empty($info)){
+                    $map['status'] = 105;
+                    $map['extent'] = 110;
+                    $map['msg']    = '手机验证码不正确';
+                    echo json_encode($map);
+                    exit;
+                }else {
+                    $codeArr = explode(',', $info['url']);
+                    $code = $codeArr[0];
+                    if($code == $verify_code){
+                        $map['status'] = 107;
+                        $map['extent'] = 110;
+                        $map['msg']    = '手机验证码已过期';
+                        echo json_encode($map);
+                        exit;
+                    }else {
+                        $map['status'] = 105;
+                        $map['extent'] = 110;
+                        $map['msg']    = '手机验证码不正确';
+                        echo json_encode($map);
+                        exit;
+                    }
+                }  
             }
-            
-            
             if($verify_code != $cache_verify_code){
                 $map['status'] = 105;
                 $map['extent'] = 110;
