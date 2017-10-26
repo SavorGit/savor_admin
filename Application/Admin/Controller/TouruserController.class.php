210da52afeb83c318ab98b21b72ee0c7ff2535fd
xiaobaig0407 <xiaobaig0407@163.com>
2017-10-26 10:49:51 +0800

    修改相关文件

--- Application/Admin/Controller/TouruserController.class.php
+++ Application/Admin/Controller/TouruserController.class.php
@@ -12,8 +12,41 @@ class TouruserController extends BaseController {
 		parent::__construct();
 	}
 
-	public function changeRepairInfo($box_infod, $damagetype){
-		$box_info = $box_infod['list'];
+	public function getRepairBox($userid, $beg_time, $end_time, $damage_type, $start, $size){
+		$redMo = new \Admin\Model\RepairBoxUserModel();
+		$field = " sys.remark username,sru.id rpid,
+                sru.remark,
+                sru.state,sru.create_time ctime,sru.type boxtype,sbo.name
+                mac_name,sru.datetime,sru.hotel_id,sru.mac,sht.name
+                hotel_name,GROUP_CONCAT(sdetail.repair_type) rtype ";
+		if ( $userid ) {
+			$condition = ' 1=1 and sru.userid ='.$userid;
+
+		} else {
+			$condition = ' 1=1 ';
+
+		}
+
+		if($beg_time)   {
+			$condition.=" AND sru.create_time>='$beg_time'";
+			$this->assign('start_date',$beg_time);
+		}
+
+		if($end_time) {
+			$condition.=" AND sru.create_time<='$end_time 23:59:59'";
+			$this->assign('end_date', $end_time);
+		}
+		if($damage_type) {
+			$condition.=" AND sdetail.repair_type = ".$damage_type;
+		}
+		$order = " sru.id DESC ";
+		$box_info = $redMo->getRepairDetail($field, $condition, $order, $start, $size);
+		return $box_info;
+	}
+
+
+	public function changeRepairDetail($box_infod){
+		$box_info = $box_infod;
 		$rdeitalModel = new \Admin\Model\RepairDetailModel();
 
 		foreach ($box_info as $bk=>$bv) {
@@ -34,36 +67,17 @@ class TouruserController extends BaseController {
 			//获取出错条件
 			$rinfo = $rdeitalModel->fetchDataWhere(array('repair_id'=>$bv['rpid']),'','repair_type',2);
 			$dam_str = '';
-			if($damagetype == 0) {
-				if ( empty($rinfo) ) {
-					$box_info[$bk]['repair_error'] = '';
-				} else {
-					$repair_arr =  C('HOTEL_DAMAGE_CONFIG');
-					foreach ($rinfo as $rv) {
-						$dam_str .= $repair_arr[$rv['repair_type']].',';
-					}
-					$dam_str = substr($dam_str,0 , -1);
-					$box_info[$bk]['repair_error'] = $dam_str;
-				}
-			} else {
-				if ( empty($rinfo) ) {
-					unset($box_info[$bk]);
-				} else {
-					$tmp_damage = array();
-					$repair_arr =  C('HOTEL_DAMAGE_CONFIG');
-					foreach ($rinfo as $rv) {
-						$dam_str .= $repair_arr[$rv['repair_type']].',';					$tmp_damage[] = $rv['repair_type'];
-
-					}
-					if(in_array($damagetype, $tmp_damage)) {
-						$dam_str = substr($dam_str,0 , -1);
-						$box_info[$bk]['repair_error'] = $dam_str;
-					} else {
-						unset($box_info[$bk]);
-					}
-				}
+			if ( empty($rinfo) ) {
+				$box_info[$bk]['repair_error'] = '';
 			}
-
+			else {
+				$repair_arr =  C('HOTEL_DAMAGE_CONFIG');
+				foreach ($rinfo as $rv) {
+					$dam_str .= $repair_arr[$rv['repair_type']].',';
+				}
+				$dam_str = substr($dam_str,0 , -1);
+				$box_info[$bk]['repair_error'] = $dam_str;
+			}
 		}
 		foreach ($box_info as $key => $row)
 		{
@@ -72,41 +86,6 @@ class TouruserController extends BaseController {
 		array_multisort($volumbe, SORT_DESC, $box_info);
 		return $box_info;
 	}
-
-
-	public function getRepairBoxInfo($userid, $beg_time, $end_time){
-		$redMo = new \Admin\Model\RepairBoxUserModel();
-		$field = " sys.remark username,sru.id rpid,
-                sru.remark,
-                sru.state,sru.create_time ctime,sru.type boxtype,sbo.name
-                mac_name,sru.datetime,sru.hotel_id,sru.mac,sht.name
-                hotel_name ";
-		if ( $userid ) {
-			$condition = ' 1=1 and sru.userid ='.$userid;
-
-		} else {
-			$condition = ' 1=1 ';
-
-		}
-
-		if($beg_time)   {
-			$condition.=" AND sru.create_time>='$beg_time'";
-			$this->assign('start_date',$beg_time);
-		}
-
-		if($end_time) {
-			$condition.=" AND sru.create_time<='$end_time 23:59:59'";
-			$this->assign('end_date', $end_time);
-		}
-		$order = " CONCAT(sru.DATETIME,sru.create_time) DESC ";
-		$box_info = $redMo->getRepairInfo($field, $condition, $order);
-		return $box_info;
-	}
-
-
-
-
-
 
 
 	/**
@@ -121,6 +100,7 @@ class TouruserController extends BaseController {
 		$beg_time = I('start_date','');
 		$end_time = I('end_date','');
 		$userid = I('userinfo','0');
+		$start  = ( $start-1 ) * $size;
 		$damage_type = I('damage_type','0');
 		if(!empty($end_time) && !empty($beg_time)) {
 			if($beg_time > $end_time) {
@@ -128,18 +108,10 @@ class TouruserController extends BaseController {
 				$this->error($msg);
 			}
 		}
-        //获取维修机顶盒人员
-		$box_info = $this->getRepairBoxInfo($userid, $beg_time, $end_time);
-		$box_info = $this->changeRepairInfo($box_info, $damage_type);
-		//var_dump($box_info);
-		$start  = ( $start-1 ) * $size;
-		//数组分页
-		$len = count($box_info);
-		$objPage = new \Common\Lib\Page($len,$size);
-		$show = $objPage->admin_page();
-		$result['page'] = $show;
-		$result['list'] = array_slice($box_info, $start, $size);
-		//获取运维组人员
+		//获取维修机顶盒人员
+		$box_info = $this->getRepairBox($userid, $beg_time, $end_time, $damage_type, $start, $size);
+		$box_detail = $box_info['list'];
+		$box_detail = $this->changeRepairDetail($box_detail);
 		$sysusergroup  = new \Admin\Model\SysusergroupModel();
 		$map['sgr.name'] = '酒楼运维';
 		$map['su.status'] = '1';
@@ -148,8 +120,8 @@ class TouruserController extends BaseController {
 		//获取损坏类型
 		$damage_config = C('HOTEL_DAMAGE_CONFIG');
 		$this->assign('damagetype', $damage_type );
-	    $this->assign('list',$result['list']);
-	    $this->assign('page',$result['page']);
+	    $this->assign('list',$box_detail);
+	    $this->assign('page',$box_info['page']);
 	    $this->assign('damage_arr',$damage_config);
 	    $this->assign('user_arr', $userarr);
 		$this->assign('useride',$userid);
--- Application/Admin/Model/RepairBoxUserModel.class.php
+++ Application/Admin/Model/RepairBoxUserModel.class.php
@@ -12,23 +12,33 @@ class RepairBoxUserModel extends BaseModel
 {
 	protected $tableName='repair_box_user';
 
-	public function getRepairInfo($field, $where, $order) {
+	
+
+	public function getRepairDetail($field, $where, $order, $start, $size) {
 		//上拉
 		$where .= " and sru.flag = 0 and (sbo.flag=0 or sbo.flag is null)";
 
-		$sql = "select ".$field." FROM savor_repair_box_user
-		sru JOIN savor_sysuser sys ON sys.id = sru.userid
-		left JOIN savor_box sbo ON sbo.mac = sru.mac JOIN
-		savor_hotel sht ON sht.id = sru.hotel_id where ".$where." order by ".$order;
+		$sql = "SELECT ".$field." FROM savor_repair_box_user sru
+		JOIN savor_sysuser sys ON sys.id = sru.userid
+		LEFT JOIN savor_box sbo ON sbo.mac = sru.mac JOIN savor_hotel
+		 sht ON sht.id = sru.hotel_id LEFT JOIN savor_repair_detail
+		 sdetail ON sru.id = sdetail.repair_id WHERE ".$where."
+		 GROUP BY sru.id ORDER BY ".$order." limit ". $start.','
+			.$size;
 		$result = $this->query($sql);
-/*
-		$sqlb = "select count(*) number FROM savor_repair_box_user
-		sru JOIN savor_sysuser sys ON sys.id = sru.userid
-		left JOIN savor_box sbo ON sbo.mac = sru.mac JOIN
-		savor_hotel sht ON sht.id = sru.hotel_id where ".$where;
+
+		$sqlb = "SELECT count(*) number FROM savor_repair_box_user sru
+		JOIN savor_sysuser sys ON sys.id = sru.userid
+		LEFT JOIN savor_box sbo ON sbo.mac = sru.mac JOIN savor_hotel
+		 sht ON sht.id = sru.hotel_id LEFT JOIN savor_repair_detail
+		 sdetail ON sru.id = sdetail.repair_id WHERE ".$where."
+		 GROUP BY sru.id";
+
 		$count_arr =  $this->query($sqlb);
-		$count = $count_arr[0]['number'];*/
-		$data = array('list'=>$result);
+		$count = count($count_arr);
+		$objPage = new Page($count,$size);
+		$show = $objPage->admin_page();
+		$data = array('list'=>$result,'page'=>$show);
 		return $data;
 	}
 
