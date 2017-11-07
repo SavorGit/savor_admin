78d328cbf6debf88cbe6e573b0eeac4625584f75
zhangyingtao75144 <125929019@qq.com>
2017-11-07 09:58:59 +0800

    运维客户端推送异常机顶盒去掉黑名单的盒子

--- Application/Admin/Model/BlackListModel.class.php
+++ Application/Admin/Model/BlackListModel.class.php
@@ -25,4 +25,14 @@ class BlackListModel extends Model
 	    $data = array('list'=>$list,'page'=>$show);
 	    return $data;
 	}
+	public function countBlackBoxNum(){
+	    $yestoday_time = strtotime('-1 day');
+	    $yestoday_start = date('Y-m-d 00:00:00',$yestoday_time);
+	    $yestoday_end   = date('Y-m-d 23:59:59',$yestoday_time);
+	    $where = array();
+	    $where['create_time'] = array('EGT',$yestoday_start);
+	    $where['create_time'] = array('ELT',$yestoday_end);
+	    $nums = $this->where($where)->count();
+	    return $nums;
+	}
 }
