a084e7aa0a38e440f21b7cd6ac58a831be1e1562
zhangyingtao75144 <125929019@qq.com>
2017-11-07 16:40:20 +0800

    推送机顶盒一场数去掉黑名单数

--- Application/Admin/Model/BlackListModel.class.php
+++ Application/Admin/Model/BlackListModel.class.php
@@ -30,8 +30,8 @@ class BlackListModel extends Model
 	    $yestoday_start = date('Y-m-d 00:00:00',$yestoday_time);
 	    $yestoday_end   = date('Y-m-d 23:59:59',$yestoday_time);
 	    $where = array();
-	    $where['create_time'] = array('EGT',$yestoday_start);
-	    $where['create_time'] = array('ELT',$yestoday_end);
+	    $where = array();
+        $where =" create_time >='".$yestoday_start."' and create_time<='".$yestoday_end."'";
 	    $nums = $this->where($where)->count();
 	    return $nums;
 	}
