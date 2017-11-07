8cf0685d584b7cc408862d2e101a4bf2f4518695
xiaobaig0407 <xiaobaig0407@163.com>
2017-11-07 11:18:40 +0800

    修改黑名单选取昨天

--- Application/Admin/Controller/BlacklistController.class.php
+++ Application/Admin/Controller/BlacklistController.class.php
@@ -22,8 +22,9 @@ class BlacklistController extends BaseController{
         $this->assign('_sort',$sort);
         $orders = $order.' '.$sort;
         $start  = ( $start-1 ) * $size;
-        
-        $where = '';
+        $yestoday = date("Y-m-d", strtotime("-1 day"));
+        $where = '1=1';
+        $where .= " and DATE_FORMAT(`create_time`,'%Y-%m-%d') = '".$yestoday."'";
         $black_list = new \Admin\Model\BlackListModel();
 		$list= $black_list->getList($where,$orders,$start,$size);
 		$m_box= new \Admin\Model\BoxModel();
