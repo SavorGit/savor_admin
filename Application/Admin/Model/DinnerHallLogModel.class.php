a2cc5f80ea410bb3cdc57ee2d40ea84a74cdd1e6
zhangyingtao75144 <125929019@qq.com>
2017-12-11 11:40:09 +0800

    餐厅端日志上报

---
+++ Application/Admin/Model/DinnerHallLogModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *@author zhang.yingtao
+ *@desc   餐厅端日志上报 
+ *@since  20171211
+ */
+namespace Admin\Model;
+
+use Admin\Model\BaseModel;
+use Common\Lib\Page;
+
+class DinnerHallLogModel extends BaseModel
+{
+	protected $tableName='dinnerapp_hall_log';
+}
