337285fa727324eba849a627d665e73a1dc8717e
xiaobaig0407 <xiaobaig0407@163.com>
2018-03-14 15:58:59 +0800

    转移发送邮件文件至文件夹

--- Application/Common/Lib/phpmailerException.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Common\Lib;
-use Think\Exception;
-class phpmailerException extends Exception {
-    public function errorMessage() {
-        $errorMsg = "<strong>aa</strong><br />\n";
-        return $errorMsg;
-    }
-}
