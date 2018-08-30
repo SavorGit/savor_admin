f22be807da571fef877b0748d28f6f05d3e952b8
zhangyingtao75144 <125929019@qq.com>
2018-08-30 17:18:35 +0800

    小程序互动游戏日志

--- Application/Admin/Controller/SappforscreenController.class.php
+++ Application/Admin/Controller/SappforscreenController.class.php
@@ -117,4 +117,51 @@ class SappforscreenController extends BaseController {
 	    }
 	    $this->output('删除成功', 'sappforscreen/index', 2);
 	}
+	/**
+	 * @desc 互动游戏日志
+	 */
+	public function gameLog(){
+	    $ajaxversion   = I('ajaxversion',0,'intval');//1 版本升级酒店列表
+	    $size   = I('numPerPage',50);//显示每页记录数
+	    $this->assign('numPerPage',$size);
+	    $start = I('pageNum',1);
+	    $this->assign('pageNum',$start);
+	    $order = I('_order','a.id');
+	    $this->assign('_order',$order);
+	    $sort = I('_sort','desc');
+	    $this->assign('_sort',$sort);
+	    $orders = $order.' '.$sort;
+	    $start  = ( $start-1 ) * $size;
+	    $where = array();
+	    $where['box.flag'] = 0;
+	    $where['box.state'] =1;
+	     
+	     
+	    $hotel_name = I('hotel_name','','trim');
+	    if($hotel_name){
+	        $where['hotel.name'] = array('like',"%$hotel_name%");
+	        $this->assign('hotel_name',$hotel_name);
+	    }
+	    $box_mac    = I('box_mac','','trim');
+	    if($box_mac){
+	        $where['a.box_mac'] = $box_mac;
+	        $this->assign('box_mac',$box_mac);
+	    }
+	    
+	    $create_time = I('create_time','','trim');
+	    if($create_time){
+	        $where['a.create_time'] = array(array('EGT',$create_time.' 00:00:00'),array('ELT',$create_time.' 23:59:59'));
+	        $this->assign('create_time',$create_time);
+	    }
+	    
+	    $m_turntable_log = new \Admin\Model\Smallapp\TurntableLogModel(); 
+	    $fields = 'area.region_name,hotel.name hotel_name,room.name room_name,a.*';
+	    
+	    $list = $m_turntable_log->getList($fields,$where,$orders,$start,$size);
+	    
+	    $this->assign('list',$list['list']);
+	    $this->assign('page',$list['page']);
+	    $this->display('Report/turntablelog');
+	    
+	}
 }
---
+++ Application/Admin/Model/Smallapp/TurntableLogModel.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @desc   小程序投屏日志 
+ * @author zhang.yingtao
+ *
+ */
+namespace Admin\Model\Smallapp;
+use Think\Model;
+use Common\Lib\Page;
+class TurntableLogModel extends Model
+{
+	protected $tableName='smallapp_turntable_log';
+	
+	public function getList($fields="a.id",$where, $order='a.id desc', $start=0,$size=5){
+	    $list = $this->alias('a')
+	                 ->join('savor_box box on a.box_mac=box.mac','left')
+	                 ->join('savor_room room on room.id= box.room_id','left')
+	                 ->join('savor_hotel hotel on room.hotel_id=hotel.id','left')
+	                 ->join('savor_area_info area on hotel.area_id=area.id','left')
+	                 ->field($fields)
+            	     ->where($where)
+            	     ->order($order)
+            	     ->limit($start,$size)
+            	     ->select();
+	    
+	    $count = $this->alias('a')
+	                  ->join('savor_box box on a.box_mac=box.mac','left')
+	                  ->join('savor_room room on room.id= box.room_id','left')
+	                  ->join('savor_hotel hotel on room.hotel_id=hotel.id','left')
+	                  ->join('savor_area_info area on hotel.area_id=area.id','left')
+	                  ->where($where)->count();
+	    $objPage = new Page($count,$size);
+	    $show = $objPage->admin_page();
+	    $data = array('list'=>$list,'page'=>$show);
+	    return $data;
+	}
+	public function getWhere($fields,$where,$limit,$group){
+	    $data = $this->alias('a')
+	                 ->join('savor_box box on a.box_mac=box.mac','left')
+	                 ->join('savor_room room on box.room_id=room.id','left')
+	                 ->join('savor_hotel hotel on room.hotel_id=hotel.id','left')
+	                 ->field($fields)
+	                 ->where($where)->limit($limit)->select();
+        return $data;	    
+	}
+	public function delWhere($where,$order,$limit){
+	    $ret =  $this->where($where)->order($order)->limit($limit)->delete();
+	    return $ret;
+	}
+}
