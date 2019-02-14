6ffa5ec19b972ac1c3d2b74c04c5a30b2e7d1ddf
liubin0125 <liubin-0125@163.com>
2019-02-14 16:00:34 +0800

    新增主题订阅通知

---
+++ Application/Admin/Controller/NotifyController.class.php
@@ -0,0 +1,36 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+ * @desc 通知
+ *
+ */
+class NotifyController extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function topicSubscribe(){
+        $content = file_get_contents('php://input');
+        $log_content = date("Y-m-d H:i:s").'[resp_result]'.$content.'[client_ip]'.get_client_ip()."\n";
+        $log_file_name = APP_PATH.'Runtime/Logs/'.'hotel_subscribe_'.date("Ymd").".log";
+        @file_put_contents($log_file_name, $log_content, FILE_APPEND);
+        if(!empty($content)){
+            $res = json_decode($content,true);
+            if(!empty($res['Message'])){
+                $all_message = explode(',',$res['Message']);
+                $hotel_list = array();
+                foreach ($all_message as $v){
+                    $hotel_list[] = array('hotel_id'=>$v);
+                }
+                $redis  =  \Common\Lib\SavorRedis::getInstance();
+                $redis->select(12);
+                $vm_hotel_key = C('VM_HOTEL_LIST');
+                $redis->set($vm_hotel_key,json_encode($hotel_list));
+            }
+        }
+    }
+
+
+}
