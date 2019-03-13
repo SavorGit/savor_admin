9ff5978b9dfe26b6495c470a3f54d7fdab190280
liubin0125 <liubin-0125@163.com>
2019-03-13 11:46:49 +0800

    广告节目列表增加图片广告

--- Application/Admin/Model/AdsModel.class.php
+++ Application/Admin/Model/AdsModel.class.php
@@ -9,18 +9,9 @@ namespace Admin\Model;
 use Admin\Model\BaseModel;
 use Common\Lib\Page;
 
-class AdsModel extends BaseModel
-{
+class AdsModel extends BaseModel{
 	protected $tableName='ads';
 
-
-	/**
-	 * getadvInfo ????????????????????????
-	 * @access public
-	 * @param $hotelid ???id
-	 * @param $menuid  ?????????id
-	 * @return mixed
-	 */
 	public function getadvInfo($hotelid, $menuid){
 		$field = "media.id AS vid,
 				media.oss_addr AS name,
@@ -106,13 +97,6 @@ class AdsModel extends BaseModel
 
 	}
 
-	/**
-	 * getadsInfo ?????????????????????ads
-	 * @access public
-	 * @param $hotelid ???id
-	 * @param $menuid  ?????????id
-	 * @return mixed
-	 */
 	public function getadsInfo($menuid){
 		$field = "media.id AS vid,
 				media.oss_addr AS name,
@@ -143,41 +127,33 @@ class AdsModel extends BaseModel
 	}
 
 	public function  getupanproInfo($menuid){
-	$field = "media.id AS vid,
-				media.oss_addr AS name,
-				media.md5 AS md5,
-				'easyMd5' AS md5_type,
-				case ads.type
-				when 1 then 'ads'
-				when 2 then 'pro'
-				when 3 then 'adv' END AS type,
-				media.oss_addr AS oss_path,
-				media.duration AS duration,
-				media.surfix ,
-				item.sort_num AS sortnum,
-				item.ads_name AS chinese_name";
-	$sql = "select ".$field;
+        $field = "media.id AS vid,
+                    media.oss_addr AS name,
+                    media.md5 AS md5,
+                    'easyMd5' AS md5_type,
+                    case ads.type
+                    when 1 then 'ads'
+                    when 2 then 'pro'
+                    when 3 then 'adv' END AS type,
+                    media.oss_addr AS oss_path,
+                    media.duration AS duration,
+                    media.surfix ,
+                    item.sort_num AS sortnum,
+                    item.ads_name AS chinese_name";
+        $sql = "select ".$field;
 
-	$sql .= "  FROM savor_ads ads LEFT JOIN savor_menu_item item
-          on ads.id = item.ads_id
-        LEFT JOIN savor_media media on media.id = ads.media_id
-        where
-            ads.state=1
-            and item.menu_id=$menuid
-            and ads.type = 2";
+        $sql .= "  FROM savor_ads ads LEFT JOIN savor_menu_item item
+              on ads.id = item.ads_id
+            LEFT JOIN savor_media media on media.id = ads.media_id
+            where
+                ads.state=1
+                and item.menu_id=$menuid
+                and ads.type = 2";
 
-	$result = $this->query($sql);
-	return $result;
+        $result = $this->query($sql);
+        return $result;
+    }
 
-}
-
-	/**
-	 * getproInfo ????????????????????§ß??
-	 * @access public
-	 * @param $hotelid ???id
-	 * @param $menuid  ?????????id
-	 * @return mixed
-	 */
 	public function getproInfo($menuid){
 		$field = "media.id AS vid,
 				media.oss_addr AS name,
@@ -213,8 +189,6 @@ class AdsModel extends BaseModel
 		return $list;
 	}
 
-
-
 	public function delData($id) {
 		$delSql = "DELETE FROM `savor_mb_content` WHERE id = '{$id}'";
 		$result = $this -> execute($delSql);
@@ -233,9 +207,5 @@ class AdsModel extends BaseModel
 		$data = array('list'=>$list,'page'=>$show);
 		return $data;
 	}
-
-
-
-
 
 }
