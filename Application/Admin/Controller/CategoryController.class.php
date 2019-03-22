3b7b9e4adf8575cb6f5dd941e53f64d771e46c67
liubin0125 <liubin-0125@163.com>
2019-03-22 10:50:16 +0800

    新增小程序投屏日志增加分类

---
+++ Application/Admin/Controller/CategoryController.class.php
@@ -0,0 +1,163 @@
+<?php
+namespace Admin\Controller;
+/**
+ * @desc 分类管理
+ *
+ */
+class CategoryController extends BaseController {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function categorylist() {
+    	$keyword = I('keyword','','trim');
+    	$category_id = I('category_id',0,'intval');
+        $page = I('pageNum',1);
+        $size   = I('numPerPage',50);//显示每页记录数
+        
+        if(empty($keyword)){
+        	$where = array('parent_id'=>0);//只显示一级分类
+        	if($category_id){
+        		$where['id'] = $category_id;
+        	}
+        }else{
+        	$where['name'] = array('like',"%$keyword%");
+        	if($category_id){
+        		$where['trees'] = array('like',"%,$category_id,%");
+        	}
+        }
+        $start  = ($page-1) * $size;
+        $m_category  = new \Admin\Model\CategoryModel();
+        $result = $m_category->getCustomList($where, 'id desc', $start, $size);
+        foreach ($result['list'] as $k=>$v){
+        	$trees = $m_category->get_category_tree($v['id']);
+        	if(!empty($trees)){
+        		unset($trees[0]);
+        	}
+        	$result['list'][$k]['trees'] = $trees;
+        }
+        $category = $m_category->getCategory($category_id,1);
+
+        $this->assign('category',$category);
+        $this->assign('keyword',$keyword);
+        $this->assign('datalist', $result['list']);
+        $this->assign('page',  $result['page']);
+        $this->assign('pageNum',$page);
+        $this->assign('numPerPage',$size);
+        $this->display('categorylist');
+    }
+    
+    public function categoryAdd(){
+        $id = I('id', 0, 'intval');
+        $m_category  = new \Admin\Model\CategoryModel();
+        if(IS_GET){
+        	$dinfo = array('status'=>1);
+        	if($id){
+        		$dinfo = $m_category->getInfo(array('id'=>$id));
+
+        		if(!empty($dinfo['parent_id'])){
+        			$id = $dinfo['parent_id'];
+        		}
+        	}
+        	$category = $m_category->getCategory($id);
+        	$this->assign('category',$category);
+        	$this->assign('dinfo',$dinfo);
+        	$this->display('categoryadd');
+        }else{
+        	$name = I('post.name','','trim');
+        	$category_id = I('post.category_id',0,'intval');
+        	$sort = I('post.sort',1,'intval');
+        	$status = I('post.status',1,'intval');
+
+        	if(empty($name)){
+        		$this->output('缺少必要参数!', 'category/categoryAdd', 2, 0);
+        	}
+        	$where = array('name'=>$name);
+        	if($id){
+                $where['id']= array('neq',$id);
+        		$res_category = $m_category->getInfo($where);
+        	}else{
+        		$res_category = $m_category->getInfo($where);
+        	}
+        	if(!empty($res_category)){
+        		$this->output('名称不能重复', 'category/categoryAdd', 2, 0);
+        	}
+
+        	$data = array('name'=>$name,'sort'=>$sort,'status'=>$status);
+        	if($id){
+        		if(empty($category_id)){
+        			$data['level'] = 1;
+        			$data['trees'] = ",$id,";
+        		}else{
+        			if($id!=$category_id){
+        				$data['parent_id'] = $category_id;
+	        			$res_cateinfo = $m_category->getInfo(array('id'=>$category_id));
+	        			$cate_tree = trim($res_cateinfo['trees'],',');
+	        			$cate_treearr = explode(',', $cate_tree);
+	        			$cate_treearr = array_unique($cate_treearr);
+	        			$data['level'] = count($cate_treearr) + 1;
+	        			$data['trees'] = $res_cateinfo['trees']."$id,";
+        			}
+        		}
+        		$condition = array('id'=>$id);
+        		$result = true;
+        	}else{
+        		if(empty($category_id)){
+        			$data['level'] = 1;
+        		}else{
+        			$data['parent_id'] = $category_id;
+        			$res_cateinfo = $m_category->getInfo(array('id'=>$category_id));
+        			$cate_tree = trim($res_cateinfo['trees'],',');
+        			$cate_treearr = explode(',', $cate_tree);
+        			$data['level'] = count($cate_treearr) + 1;
+        		}
+        		$result = $m_category->addData($data);
+	        	if($result){
+	        		if($data['level'] == 1){
+	        			$trees = ",$result,";
+	        		}else{
+	        			$trees = $res_cateinfo['trees']."$result,";
+	        		}
+	        		$condition = array('id'=>$result);
+	        		$data = array('trees'=>$trees);
+	        	}
+        	}
+        	if($result){
+        		$m_category->updateData($condition, $data);
+        		$this->output('操作成功', 'category/categorylist');
+        	}else{
+        		$this->output('操作失败', 'category/categoryAdd',2,0);
+        	}
+        }
+    }
+
+    public function operateStatus(){
+        $id = I('get.id',0,'intval');
+        $status = I('get.status',0,'intval');
+        $condition = array('id'=>$id);
+        $data = array('status'=>$status);
+        $m_category = new \Admin\Model\CategoryModel();
+        $m_category->updateData($condition, $data);
+        if($status==1){
+            $message = '启用成功';
+        }elseif($status==0){
+            $message = '禁用成功';
+        }else{
+            $message = '操作成功';
+        }
+        $this->output($message, 'category/categorylist',2);
+    }
+
+    public function categoryDel(){
+    	$category_id = I('get.id', 0, 'intval');
+        $m_category = new \Admin\Model\CategoryModel();
+    	$condition = array('trees'=>array('like',"%,$category_id,%"));
+    	$result = $m_category->delData($condition);
+    	if($result){
+    		$this->output('删除成功', '',2);
+    	}else{
+    		$this->output('删除失败', '',2);
+    	}
+    }
+}
---
+++ Application/Admin/Model/CategoryModel.class.php
@@ -0,0 +1,106 @@
+<?php
+namespace Admin\Model;
+use Common\Lib\Page;
+
+class CategoryModel extends BaseModel{
+    protected $tableName='category';
+
+    public function getCustomList($where,$orderby='id desc',$start=0,$size=0){
+        if($start >= 0 && $size){
+            $list = $this->where($where)->order($orderby)->limit($start, $size)->select();
+            if(isset($where['name']) && count($where)==1){
+                if(!empty($list) && $list[0]['level']==1){
+                    $category = $list[0]['id'];
+                    unset($where['name']);
+                    $where['trees'] = array('like',"%,$category,%");
+                }
+            }
+            if(isset($where['parent_id'])){
+                unset($where['parent_id']);
+            }
+            if(isset($where['id'])){
+                $category = $where['id'];
+                unset($where['id']);
+                $where['trees'] = array('like',"%,$category,%");
+            }
+            $count = $this->where($where)->count();
+            $page = new Page($count, $size);
+            $show = $page->admin_page();
+            $data = array('list'=>$list,'page'=>$show,'total'=>$count);
+        }else{
+            $data = $this->where($where)->order($orderby)->select();
+        }
+        return $data;
+    }
+
+    public function get_category_tree($category_id=0,$status=0){
+        $where = array();
+        if($status){
+            $where = array('status'=>$status);
+        }
+        if($category_id){
+            $where['trees'] = array('like',"%,$category_id,%");
+        }
+        $result = $this->getDataList('*',$where,'level asc');
+        $trees = $this->category_tree($result);
+        return $trees;
+    }
+
+    private static function category_tree($data,$pid=0) {
+        $trees = array();
+        foreach ($data as $k => $v) {
+            if(!empty($v['icon'])){
+                $v['icon'] = get_image_path($v['icon']);
+            }
+            if($v['parent_id'] == $pid){
+                $html = '';
+                $level = $v['level']-1>0?$v['level']-1:0;
+                if($level){
+                    $html = str_repeat('<em></em>', $level);
+                }
+                $v['html'] = $html;
+                $trees[] = $v;
+                $trees = array_merge_recursive($trees,self::category_tree($data,$v['id']));
+            }
+        }
+        return $trees;
+    }
+
+    public function getCategory($category_id=0,$level=0){
+        if($level==1){
+            $where = array('status'=>1,'parent_id'=>0);
+            $orderby = 'sort desc';
+            $res_category = $this->getDataList('*',$where,$orderby);
+        }else{
+            $res_category = $this->get_category_tree();
+        }
+        $category = array();
+        foreach ($res_category as $v){
+            $category_info = array('id'=>$v['id'],'name'=>$v['name'],'is_select'=>'','html'=>'');
+            if($v['id']==$category_id){
+                $category_info['is_select'] = 'selected';
+            }
+            if(isset($v['html'])){
+                $category_info['html'] = $v['html'];
+            }
+            $category[$v['id']] = $category_info;
+        }
+        return $category;
+    }
+    /**
+     * @desc 生成无限分类树
+     * @param array $data array('1'=>array('id'=>1,'name'=>'fenlei');
+     * @return array
+     */
+    public function genTree($data) {
+        $trees = array();
+        foreach ($data as $v){
+            if (isset($data[$v['parent_id']])){
+                $data[$v['parent_id']]['son'][] = &$data[$v['id']];
+            }else{
+                $trees[] = &$data[$v['id']];
+            }
+        }
+        return $trees;
+    }
+}
