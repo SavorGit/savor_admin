8d633354850f5bd66a367dad835640cc8948b9dc
liubin0125 <liubin-0125@163.com>
2019-05-20 11:46:54 +0800

    修改config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -16,8 +16,8 @@ $config = array(
     'DB_FIELDS_CACHE' 		=> true,
     'DATA_CACHE_TABLE'      =>'savor_datacache',
     //报错页面配置
-    'TMPL_ACTION_ERROR'     => 'Public:prompt', // 默认错误跳转对应的模板文件
-    'TMPL_ACTION_SUCCESS'   => 'Public:prompt', // 默认成功跳转对应的模板文件
+    'TMPL_ACTION_ERROR'     => APP_PATH.'Admin/View/Public/prompt.html', // 默认错误跳转对应的模板文件
+    'TMPL_ACTION_SUCCESS'   => APP_PATH.'Admin/View/Public/prompt.html', // 默认成功跳转对应的模板文件
 
     //日志配置
     'LOG_RECORD'            =>  false,   // 默认不记录日志
