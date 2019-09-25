2e1c95943f807d8f429fd78bdc376be8d30b0635
zhangyingtao75144 <125929019@qq.com>
2019-09-25 16:56:58 +0800

    批量开启、关闭机顶盒小程序码、极简版码开关

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -25,6 +25,63 @@ class TestController extends Controller {
         }
         print_r($data);
     }
+    public function openCloseQrcode(){
+        $is_close = I('is_close','0','intval');
+        //echo $is_close;exit;
+        if(!in_array($is_close,array(0,1))){
+            exit('参数错误');
+        }
+        $redis = SavorRedis::getInstance();
+        $redis->select(15);
+        $sql = "select box.* from savor_box box
+                left join savor_room room on box.room_id=room.id
+                left join savor_hotel hotel on room.hotel_id=hotel.id
+                where hotel.state=1 and hotel.flag=0 and box.state=1 and box.flag=0 and hotel.area_id=1";
+        $data = M()->query($sql);
+        $flag = 0;
+        foreach($data as $key=>$v){
+        
+            $sql ="update savor_box set is_sapp_forscreen=$is_close,is_open_simple=$is_close where id=".$v['id'].' limit 1';
+            //echo $sql;exit;
+            M()->execute($sql);
+        
+            $box_info = array();
+            $box_id = $v['id'];
+            $box_info['id']      = $v['id'];
+            $box_info['room_id'] = $v['room_id'];
+            $box_info['name']    = $v['name'];
+            $box_info['mac']     = $v['mac'];
+            $box_info['switch_time'] = $v['switch_time'];
+            $box_info['volum']   = $v['volum'];
+            $box_info['tag']     = $v['tag'];
+            $box_info['device_token'] = $v['device_token'];
+            $box_info['state']   = $v['state'];
+            $box_info['flag']    = $v['flag'];
+            $box_info['create_time'] = $v['create_time'];
+            $box_info['update_time'] = $v['update_time'];
+            $box_info['adv_mach']    = $v['adv_mach'];
+            $box_info['tpmedia_id']  = $v['tpmedia_id'];
+            $box_info['qrcode_type'] = $v['qrcode_type'];
+            $box_info['is_sapp_forscreen'] = $is_close;
+            $box_info['is_4g']       = $v['is_4g'];
+            $box_info['box_type']    = $v['box_type'];
+            $box_info['wifi_name']   = $v['wifi_name'];
+            $box_info['wifi_password']=$v['wifi_password'];
+            $box_info['wifi_mac']    = $v['wifi_mac'];
+            $box_info['is_open_simple'] = $is_close;
+            $box_info['is_open_interactscreenad'] = $v['is_open_interactscreenad'];
+            $box_info['is_open_signin'] = $v['is_open_signin'];
+            $box_cache_key = C('DB_PREFIX').'box_'.$box_id;
+            $redis->set($box_cache_key, json_encode($box_info));
+            $flag++;
+        }
+        if($is_close==0){
+            echo "北京地区已全部关闭";
+        }else {
+            echo "北京地区已全部打开";
+        }
+        
+    }
     public function removeHotelinfoCache(){
         $redis = SavorRedis::getInstance();
         $redis->select(15);
@@ -154,7 +211,7 @@ class TestController extends Controller {
             $box_info['room_id'] = $v['room_id'];
             $box_info['name']    = $v['name'];
             $box_info['mac']     = $v['mac'];
-            $box_info['switch_time'] = 999;
+            $box_info['switch_time'] = $v['switch_time'];
             $box_info['volum']   = $v['volum'];
             $box_info['tag']     = $v['tag'];
             $box_info['device_token'] = $v['device_token'];
