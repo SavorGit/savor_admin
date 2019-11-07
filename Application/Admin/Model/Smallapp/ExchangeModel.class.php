1ecdc244bf39879ee702e5aaf3634442e845e96a
liubin0125 <liubin-0125@163.com>
2019-11-07 19:58:06 +0800

    优化兑换列表

--- Application/Admin/Model/Smallapp/ExchangeModel.class.php
+++ Application/Admin/Model/Smallapp/ExchangeModel.class.php
@@ -1,7 +1,34 @@
 <?php
 namespace Admin\Model\Smallapp;
 use Admin\Model\BaseModel;
+use Common\Lib\Page;
 
 class ExchangeModel extends BaseModel{
 	protected $tableName='smallapp_exchange';
+
+    public function getExchangeList($fields,$where,$order,$start=0,$size=5){
+        $list = $this->alias('a')
+            ->join('savor_smallapp_goods goods on a.goods_id=goods.id','left')
+            ->join('savor_hotel hotel on a.hotel_id=hotel.id','left')
+            ->join('savor_hotel_ext ext on hotel.id=ext.hotel_id','left')
+            ->join('savor_area_info area on hotel.area_id=area.id','left')
+            ->field($fields)
+            ->where($where)
+            ->order($order)
+            ->limit($start,$size)
+            ->select();
+
+        $count = $this->alias('a')
+            ->join('savor_smallapp_goods goods on a.goods_id=goods.id','left')
+            ->join('savor_hotel hotel on a.hotel_id=hotel.id','left')
+            ->join('savor_hotel_ext ext on hotel.id=ext.hotel_id','left')
+            ->join('savor_area_info area on hotel.area_id=area.id','left')
+            ->field('a.id')
+            ->where($where)
+            ->count();
+        $objPage = new Page($count,$size);
+        $show = $objPage->admin_page();
+        $data = array('list'=>$list,'page'=>$show);
+        return $data;
+    }
 }
