1e00cba00a8e23991394b02f3dfb1ef3630d31ec
zhangyingtao75144 <125929019@qq.com>
2019-11-12 14:34:15 +0800

    新增model

---
+++ Application/Admin/Model/Integral/StaffModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @desc   销售端-任务
+ * @author zhang.yingtao
+ * @since  2019-11-01
+ */
+namespace Admin\Model\Integral;
+use Admin\Model\BaseModel;
+use Common\Lib\Page;
+class StaffModel extends BaseModel{
+	
+	protected $tableName='integral_merchant_staff';
+	
+}
