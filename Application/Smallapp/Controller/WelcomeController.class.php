5f4022cfe0498bc6e9b98ebb3fe579f82416b21a
liubin0125 <liubin-0125@163.com>
2019-12-10 11:27:01 +0800

    增加背景图

--- Application/Smallapp/Controller/WelcomeController.class.php
+++ Application/Smallapp/Controller/WelcomeController.class.php
@@ -10,7 +10,8 @@ class WelcomeController extends BaseController {
     public $welcome_resource_type = array(
         1=>'字号',
         2=>'颜色',
-        3=>'音乐'
+        3=>'音乐',
+        4=>'背景图'
     );
 
     public function resourcelist(){
@@ -22,6 +23,9 @@ class WelcomeController extends BaseController {
         $where = array();
         if($type){
             $where['type'] = $type;
+        }else{
+            unset($this->welcome_resource_type[4]);
+            $where['type'] = array('in',array_keys($this->welcome_resource_type));
         }
         $start = ($pageNum-1)*$size;
         $orderby = 'id desc';
@@ -74,6 +78,97 @@ class WelcomeController extends BaseController {
 
     public function musicadd(){
         $this->resource_add('musicadd');
+    }
+
+    public function backgroundimglist(){
+        $category_id = I('category_id',0,'intval');
+        $size = I('numPerPage',50,'intval');//显示每页记录数
+        $pageNum = I('pageNum',1,'intval');//当前页码
+
+        $m_category = new \Admin\Model\CategoryModel();
+        $category = $m_category->getCategory($category_id,1,6);
+
+        $m_backgroundimg = new \Admin\Model\Smallapp\BackgroundimgModel();
+        $where = array();
+        if($category_id){
+            $where['category_id'] = $category_id;
+        }
+        $start = ($pageNum-1)*$size;
+        $orderby = 'id desc';
+        $res_list = $m_backgroundimg->getDataList('*',$where,$orderby,$start,$size);
+        $data_list = array();
+        if(!empty($res_list['list'])){
+            $m_media = new \Admin\Model\MediaModel();
+            foreach ($res_list['list'] as $v){
+                $res_media = $m_media->getMediaInfoById($v['media_id']);
+                $v['img'] = $res_media['oss_addr'];
+                $v['category'] = $category[$v['category_id']]['name'];
+                if($v['status']==1){
+                    $v['status_str'] = '启用';
+                }else{
+                    $v['status_str'] = '禁用';
+                }
+                $data_list[] = $v;
+            }
+        }
+
+        $this->assign('category',$category);
+        $this->assign('data',$data_list);
+        $this->assign('page',$res_list['page']);
+        $this->assign('numPerPage',$size);
+        $this->assign('pageNum',$pageNum);
+        $this->display('backgroundimglist');
+    }
+
+    public function backgroundimgadd(){
+        $id = I('id',0,'intval');
+        $m_backgroundimg = new \Admin\Model\Smallapp\BackgroundimgModel();
+        if(IS_POST){
+            $category_id = I('post.category_id',0,'intval');
+            $media_id = I('post.media_id',0,'intval');
+            $status = I('post.status',0,'intval');
+            $data = array('media_id'=>$media_id,'category_id'=>$category_id,'status'=>$status);
+            if($id){
+                $result = $m_backgroundimg->updateData(array('id'=>$id),$data);
+            }else{
+                $result = $m_backgroundimg->addData($data);
+            }
+            if($result){
+                $this->output('操作成功!', 'welcome/backgroundimglist');
+            }else{
+                $this->output('操作失败', 'welcome/backgroundimglist',2,0);
+            }
+        }else{
+            $category_id = 0;
+            if($id){
+                $vinfo = $m_backgroundimg->getInfo(array('id'=>$id));
+                $category_id = $vinfo['category_id'];
+                $m_media = new \Admin\Model\MediaModel();
+                $res_media = $m_media->getMediaInfoById($vinfo['media_id']);
+                $vinfo['oss_addr'] = $res_media['oss_addr'];
+
+            }else{
+                $vinfo = array('status'=>1);
+            }
+            $m_category = new \Admin\Model\CategoryModel();
+            $category = $m_category->getCategory($category_id,1,6);
+
+            $this->assign('category',$category);
+            $this->assign('vinfo',$vinfo);
+            $this->display('backgroundimgadd');
+        }
+    }
+
+
+    public function backgroundimgdel(){
+        $id = I('get.id',0,'intval');
+        $m_background = new \Admin\Model\Smallapp\BackgroundimgModel();
+        $result = $m_background->delData(array('id'=>$id));
+        if($result){
+            $this->output('操作成功!', 'welcome/backgroundimglist',2);
+        }else{
+            $this->output('操作失败', 'welcome/backgroundimglist',2,0);
+        }
     }
 
 
