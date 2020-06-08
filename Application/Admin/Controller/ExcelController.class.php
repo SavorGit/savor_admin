301891ab4d9a11b344350a960909b8639707ede6
zhangyingtao75144 <125929019@qq.com>
2020-06-08 16:05:22 +0800

    数据导表

--- Application/Admin/Controller/ExcelController.class.php
+++ Application/Admin/Controller/ExcelController.class.php
@@ -134,6 +134,8 @@ class ExcelController extends Controller
              $tmpname ="用户链接wifi错误日志";
          }else if($filename =='user_wifi_forscreen_detail'){
              $tmpname = '用户极简版投屏日志';
+         }else if($filename == 'have_3day_forscreen_boxlist'){
+             $tmpname = '超过三天有普通版投屏的版位信息';
          }
 
 
@@ -6041,6 +6043,7 @@ on ext.food_style_id=food.id where hotel.state=1 and hotel.flag=0 and hotel.type
                 array('box_name','版位名称'),
                 array('box_mac','机顶盒mac'),
                 array('action_str','投屏动作'),
+                array('openid','openid'),
                 array('create_time','投屏时间')
             );
             $xlsName = '用户极简版投屏日志';
@@ -6048,6 +6051,137 @@ on ext.food_style_id=food.id where hotel.state=1 and hotel.flag=0 and hotel.type
         }
         $this->exportExcel($xlsName, $xlsCell, $data,$filename);
     }
+    public function haveWifiForsreenDetail(){
+        $start_date = I('start_date')?I('start_date'):'2020-06-03 00:00:00';
+        $type       = I('type',1);   //1:扫码日志  2:wifi链接错误日志 3:wifi投屏日志
+        //$box_mac = "'00226D2FB212','00226D584193','00226D58461F','00226D6555FB','00226D584138','00226D58423B','00226D65554A'";
+        $sql ="SELECT box_mac FROM `savor_smallapp_forscreen_record` WHERE create_time>='".$start_date."' and small_app_id=2 GROUP by box_mac";
+        $box_list = M()->query($sql);
+        $box_mac = '';
+        $space   = '';
+        foreach ($box_list as $key=>$v){
+            $box_mac .= $space ."'".$v['box_mac']."'";
+            $space    = ',';
+        }
+        if($type==1){//扫码日志
+            $sql ="SELECT hotel.name hotel_name,room.name room_name,box.name box_name,log.* FROM `savor_smallapp_qrcode_log` log
+                   left join savor_box box on log.box_mac= box.mac
+                   left join savor_room room on box.room_id= room.id
+                   left join savor_hotel hotel on room.hotel_id=hotel.id
+                   where log.create_time>='".$start_date."' and log.box_mac in($box_mac) and hotel.flag=0 and hotel.state=1";
+            
+            $data = M()->query($sql);
+            $xlsCell = array(
+                array('hotel_name', '酒楼名称'),
+                array('room_name','包间名称'),
+                array('box_name','版位名称'),
+                array('box_mac','机顶盒mac'),
+                array('openid','openid'),
+                array('create_time','扫码时间')
+                 
+            );
+            $xlsName = '用户扫码日志';
+            $filename = 'user_scan_qrcode_detail';
+    
+        }else if($type==2){//wifi链接错误日志
+            $sql = "SELECT hotel.name hotel_name,room.name room_name,box.name box_name,err.* FROM `savor_smallapp_wifi_err` err
+                   left join savor_box box on err.box_mac= box.mac
+                   left join savor_room room on box.room_id= room.id
+                   left join savor_hotel hotel on room.hotel_id=hotel.id
+                   where err.create_time>='".$start_date."' and err.box_mac in($box_mac) and hotel.flag=0 and hotel.state=1";
+    
+            $data = M()->query($sql);
+            $xlsCell = array(
+                array('hotel_name', '酒楼名称'),
+                array('room_name','包间名称'),
+                array('box_name','版位名称'),
+                array('box_mac','机顶盒mac'),
+                array('err_info','错误信息'),
+                array('create_time','错误时间')
+                 
+            );
+            $xlsName = '用户链接wifi错误日志';
+            $filename = 'user_link_wifi_err_detail';
+    
+        }else if($type==3){//wifi投屏日志
+            $sql ="select hotel.name hotel_name,room.name room_name,box.name box_name,log.openid,log.action,log.resource_type,log.create_time,log.box_mac from `savor_smallapp_forscreen_record` log
+                   left join savor_box box on log.box_mac=box.mac
+                   left join savor_room room on box.room_id= room.id
+                   left join savor_hotel hotel on hotel.id= room.hotel_id
+                   where log.create_time>='".$start_date."' and log.box_mac in($box_mac) and hotel.flag=0 and hotel.state=1 and log.mobile_brand!='devtools'";
+            $data = M()->query($sql);
+            foreach($data as $key=>$v){
+                if($v['action']==2 && $v['resource_type']==1){
+                    $data[$key]['action_str'] = '图片滑动';
+                }else if($v['action']==2 && $v['resource_type']==2){
+                    $data[$key]['action_str'] = '视频投屏';
+                }else if($v['action']==4){
+                    $data[$key]['action_str'] = '多图投屏';
+                }else if($v['action']==5){
+                    $data[$key]['action_str'] = '视频点播';
+                }
+            }
+            $xlsCell = array(
+                array('hotel_name', '酒楼名称'),
+                array('room_name','包间名称'),
+                array('box_name','版位名称'),
+                array('box_mac','机顶盒mac'),
+                array('action_str','投屏动作'),
+                array('openid','openid'),
+                array('create_time','投屏时间')
+            );
+            $xlsName = '用户极简版投屏日志';
+            $filename = 'user_wifi_forscreen_detail';
+        }
+        $this->exportExcel($xlsName, $xlsCell, $data,$filename);
+    }
+    /**
+     * @desc 2020-06-01开始连续超过三天有普通版投屏的版位信息
+     */
+    public function have3DayForscreenBoxList(){
+        $sql ="SELECT DATE_FORMAT(create_time,'%Y%m%d') as f_date,box_mac FROM `savor_smallapp_forscreen_record` WHERE `create_time`>'2020-06-01 00:00:00' and small_app_id=1 and mobile_brand !='devtools' and mobile_brand!='dev4gtools'";
+        $data = M()->query($sql);
+        $rts = [];
+        foreach($data as $key=>$v){
+            
+            if(!is_array($rts[$v['box_mac']])){
+                $rts[$v['box_mac']] = [];
+            }
+            if(!in_array($v['f_date'],$rts[$v['box_mac']])){
+                
+                array_push($rts[$v['box_mac']],$v['f_date']);
+            }
+        }
+        $box_mac_str = '';
+        $space       = '';
+        foreach($rts as $key=>$v){
+            if(count($rts[$key])<3){
+                unset($rts[$key]);
+            }else {
+                $box_mac_str.= $space."'".$key."'";
+                $space = ',';
+            }
+        }
+        $sql ="    select hotel.name hotel_name,room.name room_name,box.name box_name,box.mac 
+                   from savor_box box 
+                   left join savor_room room on box.room_id= room.id
+                   left join savor_hotel hotel on hotel.id= room.hotel_id
+                   where box.mac in($box_mac_str) and hotel.flag=0 and hotel.state=1";
+        $datalist = M()->query($sql);
+        $xlsCell = array(
+            array('hotel_name', '酒楼名称'),
+            array('room_name','包间名称'),
+            array('box_name','版位名称'),
+            array('mac','机顶盒mac'),
+        );
+        $xlsName = '超过三天有普通版投屏的版位信息';
+        $filename = 'have_3day_forscreen_boxlist';
+        $this->exportExcel($xlsName, $xlsCell, $datalist,$filename);
+        
+    }
+    /**
+     * @desc 超过7天失联的版位信息 每天早上8:30发送邮件
+     */
     public function lostBoxList(){
         
         $sql ="select area.region_name,hotel.name hotel_name ,room.name room_name,user.remark, 
