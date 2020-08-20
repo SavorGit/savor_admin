4d7b34ce6912418a1057cbe70dfc892df5a814e6
liubin0125 <liubin-0125@163.com>
2020-08-20 18:00:15 +0800

    增加样本酒楼ID

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -795,8 +795,9 @@ $config['TAGS_CATEGORY'] = array(
 
 $config['SAMPLE_HOTEL'] = array(
     236=>array(632,1064,955,427,1043,1039,1085,1077,
-        418,1097,1023,981,1038,962,1050,1046,1051,1037,1044
-        ),
+        418,1097,1023,981,1038,962,1050,1046,1051,1037,1044,
+        714,404,925,530,395,945,421,
+    ),
 );
 $config['COLLECT_FORSCREEN_OPENIDS'] = array('ofYZG4zmrApmvRSfzeA_mN-pHv2E'=>'郑伟','ofYZG42whtWOvSELbvxvnXHbzty8'=>'黄勇',
     'ofYZG49N0yz-cCTTgfPPEoL1F7l4'=>'鲍强强','ofYZG4xt_03ADzTTtf4QIrA1lt_c'=>'甘顺山','ofYZG43prBncpYjkYq-XaIWRlj6o'=>'吴琳',
