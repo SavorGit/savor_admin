86c89a1a7723d63c3aa41be35a27ca45becc1f04
liubin0125 <liubin-0125@163.com>
2021-11-01 14:15:37 +0800

    新增品鉴酒积分明细

--- Application/Admin/Model/Smallapp/UserIntegralrecordModel.class.php
+++ Application/Admin/Model/Smallapp/UserIntegralrecordModel.class.php
@@ -1,9 +1,35 @@
 <?php
 namespace Admin\Model\Smallapp;
 use Admin\Model\BaseModel;
+use Common\Lib\Page;
 
 class UserIntegralrecordModel extends BaseModel{
 	protected $tableName='smallapp_user_integralrecord';
 
-
+    public function getList($fields,$where,$orderby,$start=0,$size=0){
+        if($start >= 0 && $size){
+            $list = $this->alias('a')
+                ->join('savor_smallapp_user user on a.openid=user.openid','left')
+                ->field($fields)
+                ->where($where)
+                ->order($orderby)
+                ->limit($start,$size)
+                ->select();
+            $count = $this->alias('a')
+                ->join('savor_smallapp_user user on a.openid=user.openid','left')
+                ->where($where)
+                ->count();
+            $objPage = new Page($count,$size);
+            $show = $objPage->admin_page();
+            $data = array('list'=>$list,'page'=>$show,'total'=>$count);
+        }else{
+            $data = $this->alias('a')
+                ->join('savor_smallapp_user user on a.openid=user.openid','left')
+                ->field($fields)
+                ->where($where)
+                ->order($orderby)
+                ->select();
+        }
+        return $data;
+    }
 }
