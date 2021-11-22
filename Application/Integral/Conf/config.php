3a2e466ca0ea3e6d9bc425027bfc01122f344300
liubin0125 <liubin-0125@163.com>
2021-11-22 14:24:55 +0800

    修改积分系统分页显示错误

--- Application/Integral/Conf/config.php
+++ Application/Integral/Conf/config.php
@@ -1,5 +1,7 @@
 <?php
 $config = array(
+    'VAR_PAGE'=>'pageNum',
+    'SHOW_PAGE_TRACE'=>false,
     'servie_type'=>array('1'=>'基础服务','2'=>'增值服务'),
     'integral_task_type'=>array('1'=>'系统任务','2'=>'活动任务'),
     'system_task_content'=>array('1'=>'电视开机','2'=>'电视互动','3'=>'活动推广','4'=>'邀请食客评价','5'=>'打赏补贴'),
