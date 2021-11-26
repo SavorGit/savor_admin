7ada658e1b7e7eb04f24d69f6439e09bc057fec0
liubin0125 <liubin-0125@163.com>
2021-11-26 13:57:25 +0800

    更改查询盒子详细信息增加酒楼扩展信息

--- Application/Admin/Model/BoxModel.class.php
+++ Application/Admin/Model/BoxModel.class.php
@@ -445,6 +445,7 @@ class BoxModel extends BaseModel{
         $res = $this->alias('box')
             ->join('savor_room room on room.id= box.room_id','left')
             ->join('savor_hotel hotel on room.hotel_id=hotel.id','left')
+            ->join('savor_hotel_ext ext on hotel.id=ext.hotel_id','left')
             ->field($fields)
             ->where($where)
             ->group($group)
