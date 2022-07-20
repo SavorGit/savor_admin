2fa5f322fc520adcdce0c8daaf04ed6340c4d308
liubin0125 <liubin-0125@163.com>
2022-07-20 09:27:11 +0800

    增加核销现金优惠券

---
+++ Application/Admin/Model/FinanceGoodsModel.class.php
@@ -0,0 +1,6 @@
+<?php
+namespace Admin\Model;
+class FinanceGoodsModel extends BaseModel{
+
+    protected $tableName='finance_goods';
+}
--- Application/Smallapp/Controller/CouponController.class.php
+++ Application/Smallapp/Controller/CouponController.class.php
@@ -8,17 +8,28 @@ class CouponController extends BaseController {
         $size = I('numPerPage',50,'intval');//显示每页记录数
         $pageNum = I('pageNum',1,'intval');//当前页码
         $keywords = I('keywords','','trim');
+        $type = I('type',0,'intval');
+        $status = I('status',0,'intval');
 
         $m_coupon = new \Admin\Model\Smallapp\CouponModel();
         $where = array();
         if($keywords){
             $where['name'] = array('like',"%$keywords%");
         }
+        if($type){
+            $where['type'] = $type;
+        }
+        if($status){
+            $where['status'] = $status;
+        }
+
         $start = ($pageNum-1)*$size;
         $orderby = 'id desc';
         $res_list = $m_coupon->getDataList('*',$where,$orderby,$start,$size);
         $data_list = array();
         if(!empty($res_list['list'])){
+            $m_couponhotel = new \Admin\Model\Smallapp\CouponHotelModel();
+            $all_types = C('COUPON_TYPES');
             foreach ($res_list['list'] as $v){
                 $v['min_price'] = '满'.$v['min_price'].'可用';
                 $v['date_str'] = $v['start_time'].'到'.$v['end_time'];
@@ -27,9 +38,20 @@ class CouponController extends BaseController {
                 }else{
                     $v['status_str'] = '禁用';
                 }
+                $v['type_str'] = $all_types[$v['type']];
+                if($v['type']==2){
+                    $fields = "count(DISTINCT hotel_id) as num";
+                    $res_couponhotel = $m_couponhotel->getRow($fields,array('coupon_id'=>$v['id']),'id desc');
+                    $hotels = intval($res_couponhotel['num']);
+                }else{
+                    $hotels = '';
+                }
+                $v['hotels'] = $hotels;
                 $data_list[] = $v;
             }
         }
+        $this->assign('type',$type);
+        $this->assign('status',$status);
         $this->assign('keywords',$keywords);
         $this->assign('data',$data_list);
         $this->assign('page',$res_list['page']);
@@ -49,9 +71,24 @@ class CouponController extends BaseController {
             $start_time = I('post.start_time');
             $end_time = I('post.end_time');
             $status = I('post.status',0,'intval');
+            $type = I('post.type',0,'intval');
+            $use_range = I('post.use_range',0,'intval');
+            $range_finance_goods_ids = I('post.range_finance_goods_ids','');
+            $start_hour = I('post.start_hour',0,'intval');
 
             $data = array('name'=>$name,'money'=>$money,'min_price'=>$min_price,'remark'=>$remark,
-                'start_time'=>$start_time,'end_time'=>$end_time,'status'=>$status);
+                'start_time'=>$start_time,'end_time'=>$end_time,'type'=>$type,'use_range'=>$use_range,
+                'start_hour'=>$start_hour,'status'=>$status);
+            if($type==2){
+                if($use_range==2){
+                    if(empty($range_finance_goods_ids)){
+                        $this->output('请选择部分酒水', 'coupon/couponadd',2,0);
+                    }else{
+                        $goods_ids_str = join(',',$range_finance_goods_ids);
+                        $data['range_finance_goods_ids'] = ",$goods_ids_str,";
+                    }
+                }
+            }
             if($id){
                 $result = $m_coupon->updateData(array('id'=>$id),$data);
             }else{
@@ -64,11 +101,93 @@ class CouponController extends BaseController {
             }
         }else{
             $vinfo = array('status'=>1);
+            $range_finance_goods_ids = array();
             if($id){
                 $vinfo = $m_coupon->getInfo(array('id'=>$id));
+                if(!empty($vinfo['range_finance_goods_ids'])){
+                    $range_finance_goods_ids = explode(',',trim($vinfo['range_finance_goods_ids'],','));
+                }
             }
+            $m_finance_goods = new \Admin\Model\FinanceGoodsModel();
+            $res_goods = $m_finance_goods->getDataList('id,name',array('status'=>1),'id desc');
+            foreach ($res_goods as $k=>$v){
+                $select = '';
+                if(in_array($v['id'],$range_finance_goods_ids)){
+                    $select = 'selected';
+                }
+                $res_goods[$k]['select'] = $select;
+            }
+            $this->assign('goods',$res_goods);
             $this->assign('vinfo',$vinfo);
             $this->display();
+        }
+    }
+
+    public function hotelcouponadd(){
+        $coupon_id = I('coupon_id',0,'intval');
+        if(IS_POST){
+            $hbarr = $_POST['hbarr'];
+            if(empty($hbarr)){
+                $this->output('请选择酒楼','coupon/datalist',2,0);
+            }
+            $hotel_arr = json_decode($hbarr, true);
+            if(empty($hotel_arr)){
+                $this->output('请选择酒楼','coupon/datalist',2,0);
+            }
+            $m_couponhotel = new \Admin\Model\Smallapp\CouponHotelModel();
+            foreach ($hotel_arr as $v){
+                $hotel_id = $v['hotel_id'];
+                $where = array('hotel_id'=>$hotel_id,'coupon_id'=>$coupon_id);
+                $res = $m_couponhotel->where($where)->find();
+                if(empty($res)){
+                    $m_couponhotel->add($where);
+                }
+            }
+            $this->output('添加成功','coupon/datalist');
+        }else{
+            $areaModel  = new \Admin\Model\AreaModel();
+            $area_arr = $areaModel->getAllArea();
+            $m_coupon  = new \Admin\Model\Smallapp\CouponModel();
+            $dinfo = $m_coupon->getInfo(array('id'=>$coupon_id));
+            $this->assign('vinfo', $dinfo);
+            $this->assign('areainfo', $area_arr);
+            $this->display('hotelcouponadd');
+        }
+    }
+
+    public function hotelcouponlist() {
+        $coupon_id = I('coupon_id',0,'intval');
+        $keyword = I('keyword','','trim');
+        $page = I('pageNum',1);
+        $size   = I('numPerPage',50);
+
+        $where = array('a.coupon_id'=>$coupon_id);
+        if(!empty($keyword)){
+            $where['h.name'] = array('like',"%$keyword%");
+        }
+        $start  = ($page-1) * $size;
+        $fields = 'a.id,a.add_time,h.id as hotel_id,h.name as hotel_name';
+        $m_couponhotel = new \Admin\Model\Smallapp\CouponHotelModel();
+        $result = $m_couponhotel->getHotelCouponList($fields,$where,'a.id desc', $start,$size);
+        $datalist = $result['list'];
+
+        $this->assign('coupon_id',$coupon_id);
+        $this->assign('keyword',$keyword);
+        $this->assign('datalist', $datalist);
+        $this->assign('page',  $result['page']);
+        $this->assign('pageNum',$page);
+        $this->assign('numPerPage',$size);
+        $this->display('hotelcouponlist');
+    }
+
+    public function hotelcoupondel(){
+        $id = I('get.id',0,'intval');
+        $m_couponhotel = new \Admin\Model\Smallapp\CouponHotelModel();
+        $result = $m_couponhotel->delData(array('id'=>$id));
+        if($result){
+            $this->output('操作成功!', 'coupon/hotelcouponlist',2);
+        }else{
+            $this->output('操作失败', 'coupon/hotelcouponlist',2,0);
         }
     }
 
