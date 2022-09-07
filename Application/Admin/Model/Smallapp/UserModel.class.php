6bf535cefc3ab43bc4bcf9dce055d5514e350c34
liubin0125 <liubin-0125@163.com>
2022-09-07 10:54:49 +0800

    优化代码

--- Application/Admin/Model/Smallapp/UserModel.class.php
+++ Application/Admin/Model/Smallapp/UserModel.class.php
@@ -1,37 +1,36 @@
 <?php
-/**
- * @desc   小程序用户
- * @author zhang.yingtao
- *
- */
 namespace Admin\Model\Smallapp;
 use Think\Model;
 use Common\Lib\Page;
-class UserModel extends Model
-{
-	protected $tableName='smallapp_user';
-	public function addInfo($data,$type=1){
+class UserModel extends Model{
+
+    protected $tableName='smallapp_user';
+
+    public function addInfo($data,$type=1){
 	    if($type==1){
 	        $ret = $this->add($data);
 	         
-	    }else {
+	    }else{
 	        $ret = $this->addAll($data);
 	    }
 	    return $ret;
 	}
+
 	public function updateInfo($where,$data){
 	    $ret = $this->where($where)->save($data);
 	    return $ret;
 	}
+
 	public function getWhere($fields,$where,$order,$limit,$group){
-	    
 	    $data = $this->field($fields)->where($where)->order($order)->group($group)->limit($limit)->select();
 	    return $data;
 	}
+
 	public function getOne($fields,$where,$order){
-	    $data =  $this->field($fields)->where($where)->order($order)->find();
+	    $data = $this->field($fields)->where($where)->order($order)->find();
 	    return $data;
 	}
+
 	public function countNum($where){
 	    $nums = $this->where($where)->count();
 	    return $nums;
