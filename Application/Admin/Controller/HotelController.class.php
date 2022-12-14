67017eb8d3283b4327d308c94a74953bd4ccf6d7
zhangyingtao75144 <125929019@qq.com>
2022-12-14 15:41:29 +0800

    新增酒楼合作状态

--- Application/Admin/Controller/HotelController.class.php
+++ Application/Admin/Controller/HotelController.class.php
@@ -477,6 +477,7 @@ class HotelController extends BaseController {
 			$vinfo['is_salehotel'] = $main_info['is_salehotel'];
 			$vinfo['main_id'] = $main_info['maintainer_id'];
 			$vinfo['trade_area_type'] = $main_info['trade_area_type'];
+			$vinfo['cooperate_status'] = $main_info['cooperate_status'];
             $trainer_id = $main_info['trainer_id'];
 			if(!empty($vinfo['media_id'])){
 				$mediaModel = new \Admin\Model\MediaModel();
@@ -864,6 +865,7 @@ class HotelController extends BaseController {
 		$data['food_style_id']   = I('post.food_style_id',0,'intval');
 		$data['avg_expense']     = I('post.avg_expense',0,'intval');
 		$data['hotel_cover_media_id'] = I('post.hotel_cover_media_id',0,'intval');
+		$data['cooperate_status'] = I('post.cooperate_status',0,'intval');
 		$contract_expiretime = I('post.contract_expiretime','');
 		if($contract_expiretime){
 		    $data['contract_expiretime'] = $contract_expiretime;
