7a544df230cb4a99531054a1fa1c799a7cdafb77
liubin0125 <liubin-0125@163.com>
2023-02-13 15:46:16 +0800

    增加重新计算更新移动平均价方法

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -3605,6 +3605,8 @@ from savor_smallapp_static_hotelassess as a left join savor_hotel_ext as ext on 
     }
 
     public function avgprice(){
+        //暂时废弃
+        exit;
         $m_goods = new \Admin\Model\FinanceGoodsModel();
         $res_goods = $m_goods->getDataList('*',array(),'id asc');
         $m_goods_avgprice = new \Admin\Model\FinanceGoodsAvgpriceModel();
@@ -3628,6 +3630,105 @@ from savor_smallapp_static_hotelassess as a left join savor_hotel_ext as ext on 
             $m_goods_avgprice->add(array('goods_id'=>$goods_id,'price'=>$price));
             echo "goods_id:$goods_id,price:$price ok \r\n";
         }
+    }
+
+    public function avgprices(){
+        $m_qrcode_content = new \Admin\Model\FinanceQrcodeContentModel();
+        $m_goods = new \Admin\Model\FinanceGoodsModel();
+        $m_stock_record = new \Admin\Model\FinanceStockRecordModel();
+        $m_purchase_detail = new \Admin\Model\FinancePurchaseDetailModel();
+        $m_avgprice = new \Admin\Model\FinanceGoodsAvgpriceModel();
+
+        $res_goods = $m_goods->getDataList('*',array(),'id asc');
+        $all_goods_avg_prices = array();
+        foreach ($res_goods as $v){
+            $goods_id = $v['id'];
+
+            $sql = "select a.goods_id,sum(a.total_amount) as total_num,a.price,a.stock_id,a.stock_detail_id,stock.purchase_id,detail.purchase_detail_id from savor_finance_stock_record as a 
+                left join savor_finance_stock as stock on a.stock_id=stock.id
+                left join savor_finance_stock_detail as detail on a.stock_detail_id=detail.id
+                where a.goods_id={$goods_id} and a.type=1 and stock.io_type=11 group by a.stock_detail_id";
+            $res_data = $m_stock_record->query($sql);
+            $goods_data = array();
+            foreach ($res_data as $gk=>$gv){
+                $stock_detail_id = $gv['stock_detail_id'];
+                $in_where = array('goods_id'=>$goods_id,'stock_detail_id'=>$stock_detail_id,'type'=>1);
+                $res_inend = $m_stock_record->getDataList('idcode,add_time,amount,total_amount',$in_where,'id desc');
+                $idcodes = array();
+                foreach ($res_inend as $inv){
+                    if($inv['total_amount']==$inv['amount']){
+                        $idcodes[]=$inv['idcode'];
+                    }else{
+                        $idcodes[]=$inv['idcode'];
+                        $qrcontent = decrypt_data($inv['idcode']);
+                        $qr_id = intval($qrcontent);
+                        $res_allqrcode = $m_qrcode_content->getDataList('id',array('parent_id'=>$qr_id),'id asc');
+                        foreach ($res_allqrcode as $qrv){
+                            $qrcontent = encrypt_data($qrv['id']);
+                            $idcodes[]=$qrcontent;
+                        }
+                    }
+                }
+                $gv['in_time'] = $res_inend[0]['add_time'];
+                $gv['idcodes'] = $idcodes;
+                $goods_data[]=$gv;
+            }
+            sortArrByOneField($goods_data,'in_time',false);
+
+            $now_goods_avgprices = array();
+            foreach ($goods_data as $gdk=>$gdv){
+                $num = $gdv['total_num'];
+                $now_idcodes = $gdv['idcodes'];
+
+                $res_pd = $m_purchase_detail->getInfo(array('id'=>$gdv['purchase_detail_id']));
+                $now_price = sprintf("%.2f",$res_pd['total_fee']/$res_pd['total_amount']);
+                if($gdk==0){
+                    $avg_price = ($num*$now_price)/$num;
+                    $stock_num = 0;
+                }else{
+                    $last_total_num = $goods_data[$gdk-1]['total_num'];
+                    $last_stock_idcodes = $goods_data[$gdk-1]['idcodes'];
+                    $start_time = $goods_data[$gdk-1]['in_time'];
+                    $end_time = $gdv['in_time'];
+
+                    $wo_where = array('goods_id'=>$goods_id,'idcode'=>array('in',$last_stock_idcodes),'type'=>7,'wo_status'=>array('in','1,2,4'));
+//                    $wo_where['add_time'] = array(array('egt',$start_time),array('elt',$end_time));
+                    $wo_fields = 'sum(total_amount) as total_num';
+                    $res_wo_num = $m_stock_record->getAll($wo_fields,$wo_where,0,1);
+                    $wo_num = 0;
+                    if(!empty($res_wo_num[0]['total_num'])){
+                        $wo_num = abs($res_wo_num[0]['total_num']);
+                    }
+                    $re_where = array('goods_id'=>$goods_id,'idcode'=>array('in',$last_stock_idcodes),'type'=>6,'status'=>array('in','1,2'));
+//                    $re_where['add_time'] = array(array('egt',$start_time),array('elt',$end_time));
+                    $re_fields = 'sum(total_amount) as total_num';
+                    $res_re_num = $m_stock_record->getAll($re_fields,$re_where,0,1);
+                    $report_num = 0;
+                    if(!empty($res_re_num[0]['total_num'])){
+                        $report_num = abs($res_re_num[0]['total_num']);
+                    }
+                    $stock_num  = $last_total_num-$wo_num-$report_num;
+                    $stock_num = $stock_num + $now_goods_avgprices[$gdk-1]['stock_num'];
+                    $last_avg_price = $now_goods_avgprices[$gdk-1]['avg_price'];
+
+                    $avg_price = ($num*$now_price+$stock_num*$last_avg_price)/($num+$stock_num);
+                    $avg_price = sprintf("%.2f",$avg_price);
+                }
+                $now_goods_avgprices[$gdk] = array('avg_price'=>$avg_price,'purchase_detail_id'=>$gdv['purchase_detail_id'],
+                    'stock_detail_id'=>$gdv['stock_detail_id'],'stock_num'=>$stock_num);
+                //更新savor_finance_stock_record表中 移动平均价avg_price
+//                $m_stock_record->updateData(array('idcode'=>array('in',$now_idcodes)),array('avg_price'=>$avg_price));
+            }
+
+//            foreach ($now_goods_avgprices as $ngav){
+//                $avg_data = array('goods_id'=>$goods_id,'stock_detail_id'=>$ngav['stock_detail_id'],
+//                    'purchase_detail_id'=>$ngav['purchase_detail_id'],'price'=>$ngav['avg_price']);
+//                $m_avgprice->add($avg_data);
+//            }
+            echo "goods_id:{$goods_id} ok \r\n";
+            $all_goods_avg_prices[$goods_id] = $now_goods_avgprices;
+        }
+        print_r($all_goods_avg_prices);
     }
 
 }
