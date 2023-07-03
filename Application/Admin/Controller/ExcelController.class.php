36daa0af04bae5a63b32439007282d899ba7d4ec
zhangyingtao75144 <125929019@qq.com>
2023-07-03 13:54:59 +0800

    设备心跳与节目单更新状态表 -添加收件人 陈灵玉

--- Application/Admin/Controller/ExcelController.class.php
+++ Application/Admin/Controller/ExcelController.class.php
@@ -6516,6 +6516,7 @@ on ext.food_style_id=food.id where hotel.state=1 and hotel.flag=0 and hotel.type
             $mail->AddAddress("zhang.lijuan@littlehotspot.com");
             $mail->AddAddress("he.yongrui@littlehotspot.com");
             $mail->AddAddress("yang.kai@littlehotspot.com");
+			$mail->AddAddress("chen.lingyu@littlehotspot.com");
             $mail->AddAddress("zhang.yingtao@littlehotspot.com");
             
             $mail->IsHTML(true);
