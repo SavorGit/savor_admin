6986429d70758257aedcd8c00d1624b343d06d5b
zhangyingtao75144 <125929019@qq.com>
2023-07-25 16:43:59 +0800

    1、运维端 销售记录报表 2、回款报表

---
+++ Application/Dataexport/Controller/ReturnedmoneyController.class.php
@@ -0,0 +1,52 @@
+<?php
+namespace Dataexport\Controller;
+
+class ReturnedmoneyController extends BaseController{
+    
+    public function datalist(){
+        //set_time_limit(360);
+        //ini_set("memory_limit","1024M");
+        
+        
+        $start_date = I('start_date',date('Y-m-d',strtotime('-7 days')));
+        $end_date = I('end_date',date('Y-m-d',strtotime('-1 day')));
+        //echo $end_date;exit;
+        $init_start_date = date('Y-m-d',strtotime('-3 months'));
+        if($start_date<$init_start_date){
+            exit('只支持导出最近三个月数据');
+        }
+        $start_date .= ' 00:00:00';
+        $end_date   .= ' 23:59:59';
+        $where = "  a.pay_time>='".$start_date."' and a.pay_time<='".$end_date."'";
+        
+        $sql = "select hotel.id hotel_id,hotel.name hotel_name,user1.remark as sign_user,user2.remark residenter_user 
+                from savor_finance_sale_payment a 
+                left join savor_hotel      hotel on a.hotel_id        = hotel.id
+                left join savor_hotel_ext  ext   on hotel.id          = ext.hotel_id
+                left join savor_sysuser    user1 on ext.signer_id     = user1.id
+                left join savor_sysuser    user2 on ext.residenter_id = user2.id
+                where $where group by a.hotel_id";
+        
+        $data = M()->query($sql);
+        foreach($data as $key=>$v){
+            $map  = '';
+            $map .= $where;
+            $map .= ' and a.hotel_id='.$v['hotel_id'];
+            $sql  = "select sum(pay_money) as total_money from savor_finance_sale_payment a where ".$map;
+            $ret = M()->query($sql);
+            $data[$key]['total_money'] = $ret[0]['total_money'];
+            
+        }
+        
+        $cell = array(
+            array('hotel_id','餐厅ID'),
+            array('hotel_name','餐厅名称'),
+            array('sign_user','签约人'),
+            array('residenter_user','驻店人'),
+            array('total_money','餐厅售卖回款额'),
+            
+        );
+        $filename = '回款报表';
+        $this->exportToExcel($cell,$data,$filename,1);
+    }
+}
