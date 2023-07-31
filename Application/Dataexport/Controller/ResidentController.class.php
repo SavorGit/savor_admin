f8e246992837b9cf96cb18f30137948ce56177bf
liubin0125 <liubin-0125@163.com>
2023-07-31 09:23:15 +0800

    优化代码

--- Application/Dataexport/Controller/ResidentController.class.php
+++ Application/Dataexport/Controller/ResidentController.class.php
@@ -9,10 +9,14 @@ class ResidentController extends BaseController{
         $m_static_resident = new \Admin\Model\StaticResidentModel();
         $field = 'area_id,area_name,residenter_id,residenter_name,sum(num) as num,avg(task_demand_finish_rate) as task_demand_finish_rate,
         avg(task_invitation_finish_rate) as task_invitation_finish_rate,sum(sale_money) as sale_money';
-        $datalist = $m_static_resident->getAllData($field,array('static_month'=>$static_month),'','signer_id');
+        $datalist = $m_static_resident->getAllData($field,array('static_month'=>$static_month),'','residenter_id');
+        foreach ($datalist as $k=>$v){
+            $datalist[$k]['task_demand_finish_rate'] = round($v['task_demand_finish_rate'],2);
+            $datalist[$k]['task_invitation_finish_rate'] = round($v['task_invitation_finish_rate'],2);
+        }
         $cell = array(
             array('area_name','城市'),
-            array('signer_name','驻店人'),
+            array('residenter_name','驻店人'),
             array('num','销售量'),
             array('task_demand_finish_rate','点播任务完成率'),
             array('task_invitation_finish_rate','邀请函任务完成率'),
