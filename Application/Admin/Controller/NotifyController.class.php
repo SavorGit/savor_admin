8bd684e48b57f22fd08fc74e0a0bd78675ac16bf
liubin0125 <liubin-0125@163.com>
2023-08-02 17:14:14 +0800

    修改更新公司库存通知日志记录

--- Application/Admin/Controller/NotifyController.class.php
+++ Application/Admin/Controller/NotifyController.class.php
@@ -44,7 +44,7 @@ class NotifyController extends Controller {
             }
         }
         $log_content = date("Y-m-d H:i:s").'[resp_result]'.$content.'[params]'.json_encode($orders).'[client_ip]'.get_client_ip()."\n";
-        $log_file_name = APP_PATH.'Runtime/Logs/'.'hotel_subscribe_'.date("Ymd").".log";
+        $log_file_name = APP_PATH.'Runtime/Logs/'.'companystock_'.date("Ymd").".log";
         @file_put_contents($log_file_name, $log_content, FILE_APPEND);
         if(empty($orders[0]['order_id'])){
             return true;
