dc6aab868db1e85c8027b876e936727043c18fd6
liubin0125 <liubin-0125@163.com>
2023-08-22 19:12:01 +0800

    增加导出驻店人员工作表

--- Application/Dataexport/Controller/ResidentController.class.php
+++ Application/Dataexport/Controller/ResidentController.class.php
@@ -25,4 +25,187 @@ class ResidentController extends BaseController{
         $this->exportToExcel($cell,$datalist,$filename,1);
     }
 
+    public function selldata(){
+        $area_id = I('get.area_id',1,'intval');
+        $month = I('get.month',1,'intval');
+        $year = date('Y');
+
+        $startOfMonth = date('Y-m-01', strtotime($year . '-' . $month . '-01'));
+        $endOfMonth = date('Y-m-t', strtotime($year . '-' . $month . '-01'));
+        /*
+        $startDate = new \DateTime($startOfMonth);
+        $endDate = new \DateTime($endOfMonth);
+        $interval = \DateInterval::createFromDateString('1 day');
+        $period = new \DatePeriod($startDate, $interval, $endDate);
+        $weeks = array();
+        foreach ($period as $date) {
+            $weekNumber = $date->format('W');
+            if (!isset($weeks[$weekNumber])) {
+                $weeks[$weekNumber] = array(
+                    'start' => $date->format('Y-m-d'),
+                    'end' => $date->format('Y-m-d'),
+                );
+            } else {
+                $weeks[$weekNumber]['end'] = $date->format('Y-m-d');
+            }
+        }
+        $weeks = array_values($weeks);
+        $week_num = count($weeks);
+        if($weeks[$week_num-1]['end']<$endOfMonth){
+            $weeks[$week_num-1]['end'] = $endOfMonth;
+        }
+        */
+        $weeks = array();
+        $first_day_of_month = date('Y-m-01');
+        $first_day_of_month_week = date("W", strtotime($first_day_of_month));
+        $current_week = date("W") - $first_day_of_month_week + 1;
+//        $current_week = 5;
+        for ($i=1; $i<=$current_week; $i++) {
+            $date = date("Y-m-d", strtotime($first_day_of_month ." +".(7 * ($i - 1)) .'days'));
+            $week_start_date = date('Y-m-d', strtotime("last monday", strtotime($date)));
+            $week_end_date = date('Y-m-d', strtotime("next sunday", strtotime($date)));
+            $weeks[]=array('start'=>$week_start_date,'end'=>$week_end_date);
+        }
+        $month_start_time = "$startOfMonth 00:00:00";
+        $month_end_time = "$endOfMonth 23:59:59";
+
+        $sql ="select a.id as hotel_id,a.name as hotel_name,a.area_id,area.region_name as area_name,circle.name circle_name,
+            ext.signer_id,ext.residenter_id,signer.remark as signer_name,residenter.remark as residenter_name
+            from savor_hotel as a left join savor_hotel_ext as ext on a.id=ext.hotel_id
+            left join savor_area_info as area on a.area_id=area.id
+            left join savor_business_circle as circle on a.business_circle_id = circle.id
+            left join savor_sysuser signer on ext.signer_id=signer.id
+            left join savor_sysuser residenter on ext.residenter_id=residenter.id
+            where a.state=1 and a.flag=0 and ext.is_salehotel=1 and a.area_id={$area_id} order by a.id desc";
+        $result = M()->query($sql);
+        $m_room = new \Admin\Model\RoomModel();
+        $m_sale = new \Admin\Model\FinanceSaleModel();
+        $m_sale_payment_record = new \Admin\Model\FinanceSalePaymentRecordModel();
+        $m_merchant = new \Admin\Model\Integral\MerchantModel();
+        $m_staff = new \Admin\Model\Integral\StaffModel();
+        $m_hotelstaff_data = new \Admin\Model\Smallapp\StaticHotelstaffdataModel();
+        $redis = new \Common\Lib\SavorRedis();
+        $redis->select(9);
+        $cache_key = C('FINANCE_HOTELSTOCK');
+        $datalist = array();
+        foreach ($result as $v){
+            $hotel_id = $v['hotel_id'];
+            $circle_name = !empty($v['circle_name'])?$v['circle_name']:'';
+            $res_room = $m_room->getRoomByCondition('count(room.id) as num',array('hotel.id'=>$v['hotel_id'],'room.state'=>1,'room.flag'=>0));
+            $room_num = intval($res_room[0]['num']);
+
+            $salewhere = array('a.hotel_id'=>$hotel_id,'record.wo_reason_type'=>1,'record.wo_status'=>2);
+            $salewhere['record.add_time'] = array(array('egt',$month_start_time),array('elt',$month_end_time));
+            $res_stock_record = $m_sale->alias('a')
+                ->field('count(a.id) as num')
+                ->join('savor_finance_stock_record record on a.stock_record_id=record.id','left')
+                ->where($salewhere)
+                ->select();
+            $sale_num = intval($res_stock_record[0]['num']);
+
+            $sale_week1_num=$sale_week2_num=$sale_week3_num=$sale_week4_num=$sale_week5_num=0;
+            foreach ($weeks as $wk=>$wday){
+                $w_stime = "{$wday['start']} 00:00:00";
+                $w_etime = "{$wday['end']} 23:59:59";
+                $salewhere['record.add_time'] = array(array('egt',$w_stime),array('elt',$w_etime));
+                $res_week_stock_record = $m_sale->alias('a')
+                    ->field('count(a.id) as num')
+                    ->join('savor_finance_stock_record record on a.stock_record_id=record.id','left')
+                    ->where($salewhere)
+                    ->select();
+                if($wk==0)  $sale_week1_num=intval($res_week_stock_record[0]['num']);
+                if($wk==1)  $sale_week2_num=intval($res_week_stock_record[0]['num']);
+                if($wk==2)  $sale_week3_num=intval($res_week_stock_record[0]['num']);
+                if($wk==3)  $sale_week4_num=intval($res_week_stock_record[0]['num']);
+                if($wk==4)  $sale_week5_num=intval($res_week_stock_record[0]['num']);
+            }
+
+            $sale_where = array('stock.hotel_id'=>$hotel_id,'record.wo_reason_type'=>1,'a.add_time'=>array(array('egt',$month_start_time),array('elt',$month_end_time)));
+            $sale_where['a.ptype'] = array('in','0,2');
+            $res_sale_qk = $m_sale->getSaleStockRecordList('a.id as sale_id,a.settlement_price,a.ptype,a.is_expire,a.add_time',$sale_where,'','');
+            $qk_money = 0;
+            $cqqk_money = 0;
+            if(!empty($res_sale_qk)){
+                foreach ($res_sale_qk as $salev){
+                    if($salev['ptype']==0){
+                        $now_money = $salev['settlement_price'];
+                    }else{
+                        $res_had_pay = $m_sale_payment_record->getRow('sum(pay_money) as total_pay_money',array('sale_id'=>$salev['sale_id']));
+                        $had_pay_money = intval($res_had_pay['total_pay_money']);
+                        $now_money = $salev['settlement_price']-$had_pay_money;
+                    }
+                    $qk_money+=$now_money;
+                    if($salev['is_expire']==1){
+                        $cqqk_money+=$now_money;
+                    }
+                }
+            }
+            $cqqk_money = abs($cqqk_money);
+
+            $res_merchant = $m_merchant->getMerchants('a.id,a.is_shareprofit',array('a.hotel_id'=>$hotel_id,'a.status'=>1),'');
+            $is_shareprofit = 0;
+            $is_shareprofit_str = '';
+            $sale_people_num = 0;
+            if(!empty($res_merchant[0]['id'])){
+                $is_shareprofit = $res_merchant[0]['is_shareprofit'];
+                if($is_shareprofit==1){
+                    $is_shareprofit_str = '是';
+                }else{
+                    $is_shareprofit_str = '否';
+                }
+
+                $res_staff_num = $m_staff->getRow('count(*) as num',array('merchant_id'=>$res_merchant[0]['id'],'status'=>1));
+                $sale_people_num = intval($res_staff_num['num']);
+            }
+
+            $staff_fields = 'sum(a.task_demand_finish_num) as task_demand_finish_num,sum(task_demand_operate_num) as task_demand_operate_num,
+            sum(a.task_invitation_finish_num) as task_invitation_finish_num,sum(a.task_invitation_operate_num) as task_invitation_operate_num';
+            $staff_where = array('a.hotel_id'=>$hotel_id,'a.static_date'=>array(array('egt',$startOfMonth),array('elt',$endOfMonth)));
+            $res_task_data = $m_hotelstaff_data->getHotelDataList($staff_fields,$staff_where);
+            $task_demand_finish_rate = sprintf("%.2f",$res_task_data[0]['task_demand_finish_num']/$res_task_data[0]['task_demand_operate_num']);
+            $task_invitation_finish_rate = sprintf("%.2f",$res_task_data[0]['task_invitation_finish_num']/$res_task_data[0]['task_invitation_operate_num']);
+
+            $sku_num = 0;
+            $res_cache_stock = $redis->get($cache_key.":$hotel_id");
+            if(!empty($res_cache_stock)){
+                $cache_stock = json_decode($res_cache_stock,true);
+                $sku_num = count($cache_stock['goods_ids']);
+            }
+
+            $datalist[]=array('hotel_id'=>$v['hotel_id'],'hotel_name'=>$v['hotel_name'],'area_name'=>$v['area_name'],
+                'circle_name'=>$circle_name,'room_num'=>$room_num,'sale_num'=>$sale_num,'sale_week1_num'=>$sale_week1_num,
+                'sale_week2_num'=>$sale_week2_num,'sale_week3_num'=>$sale_week3_num,'sale_week4_num'=>$sale_week4_num,'sale_week5_num'=>$sale_week5_num,
+                'signer_name'=>$v['signer_name'],'residenter_name'=>$v['residenter_name'],'qk_money'=>$qk_money,'cqqk_money'=>$cqqk_money,'sale_people_num'=>$sale_people_num,
+                'sku_num'=>$sku_num,'task_demand_finish_rate'=>$task_demand_finish_rate,'task_invitation_finish_rate'=>$task_invitation_finish_rate,
+                'is_shareprofit_str'=>$is_shareprofit_str,
+            );
+        }
+
+        $cell = array(
+            array('hotel_id','酒楼ID'),
+            array('hotel_name','酒楼名称'),
+            array('circle_name','商圈'),
+            array('room_num','包间数'),
+            array('sale_num','本月销量'),
+            array('sale_week1_num','1周销量'),
+            array('sale_week2_num','2周销量'),
+            array('sale_week3_num','3周销量'),
+            array('sale_week4_num','4周销量'),
+            array('sale_week5_num','5周销量'),
+            array('signer_name','签约人'),
+            array('residenter_name','驻店人'),
+            array('qk_money','总欠款'),
+            array('cqqk_money','超期欠款'),
+            array('sale_people_num','销售端人数'),
+            array('sku_num','sku数'),
+            array('task_demand_finish_rate','点播任务完成率'),
+            array('task_invitation_finish_rate','邀请函任务完成率'),
+            array('is_shareprofit_str','是否分润'),
+
+        );
+
+        $filename = '驻店人员工作表';
+        $this->exportToExcel($cell,$datalist,$filename,1);
+
+    }
 }
