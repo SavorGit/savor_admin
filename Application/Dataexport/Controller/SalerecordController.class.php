7550c7e17d222e7bf47c65cec3b820dd501e09d7
liubin0125 <liubin-0125@163.com>
2023-09-25 16:34:17 +0800

    增加导出销售记录增加职位数据

--- Application/Dataexport/Controller/SalerecordController.class.php
+++ Application/Dataexport/Controller/SalerecordController.class.php
@@ -4,13 +4,8 @@ namespace Dataexport\Controller;
 class SalerecordController extends BaseController{
     
     public function datalist(){
-        //set_time_limit(360);
-        //ini_set("memory_limit","1024M");
-        
-        
         $start_date = I('start_date',date('Y-m-d',strtotime('-7 days')));
         $end_date = I('end_date',date('Y-m-d',strtotime('-1 day')));
-        //echo $end_date;exit;
         $init_start_date = date('Y-m-d',strtotime('-3 months'));
         if($start_date<$init_start_date){
             exit('只支持导出最近三个月数据');
@@ -24,22 +19,16 @@ class SalerecordController extends BaseController{
         foreach($cate_list as $key=>$v){
             $cate_arr[$v['id']] = $v;
         }
-        
-        //secsToStr
-        
-        $where = ' 1 =1';
-        $where .=" and a.add_time>='".$start_date."' && a.add_time<='".$end_date."' and a.type=1 and a.status=2";
+        $where ="a.add_time>='".$start_date."' and a.add_time<='".$end_date."' and a.type=1 and a.status=2";
         
         $sql ="select a.ops_staff_id,area.region_name,user.remark,hotel.id hotel_id,hotel.name hotel_name,
-               signin_time,signout_time,visit_type,visit_purpose
+               a.signin_time,a.signout_time,a.visit_type,a.visit_purpose,staff.job
                from savor_crm_salerecord a
                left join savor_ops_staff  staff on a.ops_staff_id = staff.id
                left join savor_sysuser    user  on user.id = staff.sysuser_id
                left join savor_hotel      hotel on a.signin_hotel_id = hotel.id
                left join savor_area_info  area  on hotel.area_id = area.id where ".$where;
-        
         $data = M()->query($sql);
-        
         foreach($data as $key=>$v){
             $visit_purpose_str = trim($v['visit_purpose'],',');
             $tmp  = explode(',',$visit_purpose_str);
@@ -57,18 +46,14 @@ class SalerecordController extends BaseController{
         $cell = array(
             array('region_name','城市'),
             array('remark','姓名'),
-            
+            array('job','职位'),
             array('hotel_id','餐厅ID'),
             array('hotel_name','餐厅名称'),
-            
-            
             array('signin_time','签到时间'),
             array('signout_time','签退时间'),
             array('visit_duration','拜访时长'),
-            
             array('visite_type_str','拜访类型'),
             array('visit_purpose_str','拜访目的'),
-            
         );
         $filename = '运维端销售记录报表';
         $this->exportToExcel($cell,$data,$filename,1);
