4ee464eb33cff2a034eb336158a584eb9daf3c00
liubin0125 <liubin-0125@163.com>
2023-10-08 11:16:06 +0800

    增加通知-审核核销

--- Application/Admin/Controller/NotifyController.class.php
+++ Application/Admin/Controller/NotifyController.class.php
@@ -69,8 +69,28 @@ class NotifyController extends Controller {
                 break;
         }
         echo 'success';
+    }
 
+    public function auditwriteoff(){
+        $content = file_get_contents('php://input');
+        $orders = array();
+        if(!empty($content)) {
+            $res = json_decode($content, true);
+            if (!empty($res['Message'])) {
+                $message = base64_decode($res['Message']);
+                $orders = json_decode($message,true);
+            }
+        }
+        $log_content = date("Y-m-d H:i:s").'[resp_result]'.$content.'[params]'.json_encode($orders).'[client_ip]'.get_client_ip()."\n";
+        $log_file_name = APP_PATH.'Runtime/Logs/'.'writeoffdata_'.date("Ymd").".log";
+        @file_put_contents($log_file_name, $log_content, FILE_APPEND);
+        if(empty($orders[0]['order_id'])){
+            return true;
+        }
+        $shell = "/opt/install/php/bin/php /application_data/web/php/savor_admin/cli.php crontab/audit/writeoffdata > /tmp/null &";
+        system($shell);
 
+        echo 'success';
     }
 
 }
