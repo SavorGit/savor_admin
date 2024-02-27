f6f2ae4aadb2990c82239dd26d7a19cf64ad2b23
liubin0125 <liubin-0125@163.com>
2024-02-27 14:55:24 +0800

    增加导出部门,小组字段

--- Application/Admin/Model/Smallapp/HotelGoodsModel.class.php
+++ Application/Admin/Model/Smallapp/HotelGoodsModel.class.php
@@ -6,25 +6,42 @@ use Common\Lib\Page;
 class HotelGoodsModel extends BaseModel{
 	protected $tableName='smallapp_hotelgoods';
 
-    public function getHotelgoodsList($fields,$where,$order, $start=0,$size=5){
-        $list = $this->alias('a')
-            ->join('savor_hotel h on a.hotel_id=h.id','left')
-            ->join('savor_area_info area on h.area_id=area.id','left')
-            ->field($fields)
-            ->where($where)
-            ->order($order)
-            ->limit($start,$size)
-            ->select();
+    public function getHotelgoodsList($fields,$where,$order, $start=0,$size=0){
+        if($start>=0 && $size>0){
+            $list = $this->alias('a')
+                ->join('savor_smallapp_dishgoods dg on a.goods_id=dg.id','left')
+                ->join('savor_hotel h on a.hotel_id=h.id','left')
+                ->join('savor_hotel_ext ext on a.hotel_id=ext.hotel_id','left')
+                ->join('savor_area_info area on h.area_id=area.id','left')
+                ->field($fields)
+                ->where($where)
+                ->order($order)
+                ->limit($start,$size)
+                ->select();
 
-        $count = $this->alias('a')
-            ->join('savor_hotel h on a.hotel_id=h.id','left')
-            ->field('a.id')
-            ->where($where)
-            ->select();
-        $count = count($count);
-        $objPage = new Page($count,$size);
-        $show = $objPage->admin_page();
-        $data = array('list'=>$list,'page'=>$show);
+            $count = $this->alias('a')
+                ->join('savor_smallapp_dishgoods dg on a.goods_id=dg.id','left')
+                ->join('savor_hotel h on a.hotel_id=h.id','left')
+                ->join('savor_hotel_ext ext on a.hotel_id=ext.hotel_id','left')
+                ->join('savor_area_info area on h.area_id=area.id','left')
+                ->field('a.id')
+                ->where($where)
+                ->select();
+            $count = count($count);
+            $objPage = new Page($count,$size);
+            $show = $objPage->admin_page();
+            $data = array('list'=>$list,'page'=>$show);
+        }else{
+            $data = $this->alias('a')
+                ->join('savor_smallapp_dishgoods dg on a.goods_id=dg.id','left')
+                ->join('savor_hotel h on a.hotel_id=h.id','left')
+                ->join('savor_hotel_ext ext on a.hotel_id=ext.hotel_id','left')
+                ->join('savor_area_info area on h.area_id=area.id','left')
+                ->field($fields)
+                ->where($where)
+                ->order($order)
+                ->select();
+        }
         return $data;
     }
 
