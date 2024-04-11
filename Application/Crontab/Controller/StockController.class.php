afccde861b7a756089010dc503061da6c345d454
liubin0125 <liubin-0125@163.com>
2024-04-11 15:30:45 +0800

    增加更新核销回收状态更改为过期未回收脚本

---
+++ Application/Crontab/Controller/StockController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Crontab\Controller;
+use Think\Controller;
+
+class StockController extends Controller{
+
+    public function expirerecycle(){
+        $diff_time = time() - 86400*60;
+        $where = array('type'=>7,'wo_status'=>2,'recycle_status'=>1);
+        $where['UNIX_TIMESTAMP(add_time)'] = array('elt',$diff_time);
+        $m_stock_record = new \Admin\Model\FinanceStockRecordModel();
+        $m_stock_record->updateData($where,array('recycle_status'=>7));
+
+        $now_time = date('Y-m-d H:i:s');
+        echo "expirerecycle time:$now_time \r\n";
+    }
+}
