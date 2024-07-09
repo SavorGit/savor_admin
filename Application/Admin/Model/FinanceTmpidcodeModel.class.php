964d07b3adf9bfd1e0a560a48575436198e96b2b
liubin0125 <liubin-0125@163.com>
2024-07-09 09:51:57 +0800

    增加查询财务核对码数据功能

---
+++ Application/Admin/Model/FinanceTmpidcodeModel.class.php
@@ -0,0 +1,6 @@
+<?php
+namespace Admin\Model;
+class FinanceTmpidcodeModel extends BaseModel{
+
+    protected $tableName='finance_tmpidcode';
+}
--- Application/Crontab/Controller/StockController.class.php
+++ Application/Crontab/Controller/StockController.class.php
@@ -63,4 +63,170 @@ class StockController extends Controller{
         $now_time = date('Y-m-d H:i:s');
         echo "idcode end:$now_time \r\n";
     }
+
+    public function tmpidcode(){
+        $m_qrcode_content = new \Admin\Model\FinanceQrcodeContentModel();
+        $sql_1 = "SELECT a.idcode,a.price,a.avg_price,a.goods_id,a.add_time,goods.name as goods_name,area.id as area_id,area.region_name as area_name,stock.id as in_stock_id,stock.name as in_stock_name,
+            stock.serial_number as in_serial_number,p.id as purchase_id,p.serial_number as purchase_serial_number,
+            p.name as purchase_name from savor_finance_stock_record as a 
+            left join savor_finance_stock as stock on a.stock_id=stock.id
+            left join savor_area_info as area on stock.area_id=area.id
+            left join savor_finance_purchase as p on 	stock.purchase_id=p.id
+            left join savor_finance_goods as goods on a.goods_id=goods.id 
+            where a.type=1 and a.dstatus=1 and a.goods_id not in (56,62) and stock.io_type=11 and a.add_time>='2024-01-01 00:00:00' and a.total_amount>1
+            order by a.id asc ";
+        $now_time = date('Y-m-d H:i:s');
+        echo "box code begin $now_time \r\n";
+        $m_tmpidcode = new \Admin\Model\FinanceTmpidcodeModel();
+        $m_stock_record = new \Admin\Model\FinanceStockRecordModel();
+        $m_sale = new \Admin\Model\FinanceSaleModel();
+        $m_price_template_hotel = new \Admin\Model\FinancePriceTemplateHotelModel();
+        $res_data = $m_tmpidcode->query($sql_1);
+        foreach ($res_data as $v){
+            $idcode = $v['idcode'];
+            $qrcontent = decrypt_data($idcode);
+            $qr_id = intval($qrcontent);
+            $res_sondata = $m_qrcode_content->getDataList('id',array('parent_id'=>$qr_id),'id asc');
+            $son_datas = array();
+            foreach ($res_sondata as $sv){
+                $son_idcode = encrypt_data($sv['id']);
+
+                $son_fileds = 'stock.id as out_stock_id,stock.name as out_stock_name,stock.serial_number as out_serial_number,a.type,a.unit_id,a.wo_reason_type,a.wo_status,
+                stock.hotel_id,hotel.name as hotel_name';
+                $res_son_record = $m_stock_record->alias('a')
+                    ->field($son_fileds)
+                    ->join('savor_finance_stock stock on a.stock_id=stock.id','left')
+                    ->join('savor_hotel hotel on stock.hotel_id=hotel.id','left')
+                    ->where(array('a.idcode'=>$son_idcode,'a.dstatus'=>1))
+                    ->order('a.id desc')
+                    ->limit('0,1')
+                    ->select();
+
+                $sale_type = 0;
+                if(!empty($res_son_record[0]['out_stock_id'])){
+                    $v['idcode'] = $son_idcode;
+                    if($res_son_record[0]['out_stock_id']==$v['in_stock_id']){
+                        $res_son_record[0]['out_stock_id'] = 0;
+                        $res_son_record[0]['out_stock_name'] = '';
+                        $res_son_record[0]['out_serial_number'] = '';
+                    }
+                    $now_son_data = array_merge($v,$res_son_record[0]);
+                    if($res_son_record[0]['type']==7){
+                        $res_sale = $m_sale->getInfo(array('idcode'=>$son_idcode));
+                        $sale_type = $res_sale['type'];
+                        $settlement_price = $res_sale['settlement_price'];
+                    }else{
+                        $res_sale = $m_sale->getAll('type,num,settlement_price',array('idcode'=>array('like',"%$son_idcode%")),0,1,'id desc');
+                        if(!empty($res_sale[0]['type'])){
+                            $sale_type = $res_sale[0]['type'];
+                            $settlement_price = $res_sale[0]['settlement_price']/$res_sale[0]['num'];
+                        }else{
+                            $hotel_id = intval($res_son_record[0]['hotel_id']);
+                            $settlement_price = $m_price_template_hotel->getHotelGoodsPrice($hotel_id,$v['goods_id']);
+                        }
+                    }
+                    $now_son_data['settlement_price'] = $settlement_price;
+                    $now_son_data['sale_type'] = $sale_type;
+                    if(empty($now_son_data['hotel_name'])){
+                        $now_son_data['hotel_name'] = '';
+                    }
+                    $son_datas[]=$now_son_data;
+                }
+            }
+            if(!empty($son_datas)){
+                $son_num = count($son_datas);
+                $m_tmpidcode->addAll($son_datas);
+                echo "$idcode,son_num:$son_num \r\n";
+            }else{
+                $p_fileds = 'stock.id as out_stock_id,stock.name as out_stock_name,stock.serial_number as out_serial_number,a.type,a.unit_id,a.wo_reason_type,a.wo_status,
+                stock.hotel_id,hotel.name as hotel_name';
+                $res_p_record = $m_stock_record->alias('a')
+                    ->field($p_fileds)
+                    ->join('savor_finance_stock stock on a.stock_id=stock.id','left')
+                    ->join('savor_hotel hotel on stock.hotel_id=hotel.id','left')
+                    ->where(array('a.idcode'=>$idcode,'a.dstatus'=>1))
+                    ->order('a.id desc')
+                    ->limit('0,1')
+                    ->select();
+                if(!empty($res_p_record[0]['out_stock_id'])){
+                    if($res_p_record[0]['out_stock_id']==$v['in_stock_id']){
+                        $res_p_record[0]['out_stock_id'] = 0;
+                        $res_p_record[0]['out_stock_name'] = '';
+                        $res_p_record[0]['out_serial_number'] = '';
+                    }
+                    $now_p_data = array_merge($v,$res_p_record[0]);
+                    if(empty($now_p_data['hotel_name'])){
+                        $now_p_data['hotel_name'] = '';
+                    }
+                    $m_tmpidcode->add($now_p_data);
+
+                    echo "$idcode,son_num:0 \r\n";
+                }
+            }
+        }
+        $now_time = date('Y-m-d H:i:s');
+        echo "box code end $now_time \r\n";
+
+        echo "idcode start $now_time \r\n";
+
+        $sql_2 = "SELECT a.idcode,a.price,a.avg_price,a.goods_id,a.add_time,goods.name as goods_name,area.id as area_id,area.region_name as area_name,stock.id as in_stock_id,stock.name as in_stock_name,
+            stock.serial_number as in_serial_number,p.id as purchase_id,p.serial_number as purchase_serial_number,
+            p.name as purchase_name from savor_finance_stock_record as a 
+            left join savor_finance_stock as stock on a.stock_id=stock.id
+            left join savor_area_info as area on stock.area_id=area.id
+            left join savor_finance_purchase as p on 	stock.purchase_id=p.id
+            left join savor_finance_goods as goods on a.goods_id=goods.id 
+            where a.type=1 and a.goods_id not in (56,62) and stock.io_type=11 and a.add_time>='2024-01-01 00:00:00' and a.total_amount=1
+            order by a.id asc ";
+        $res_data = $m_tmpidcode->query($sql_2);
+        foreach ($res_data as $v){
+            $idcode = $v['idcode'];
+
+            $idcode_fileds = 'stock.id as out_stock_id,stock.name as out_stock_name,stock.serial_number as out_serial_number,a.type,a.unit_id,a.wo_reason_type,a.wo_status,
+                stock.hotel_id,hotel.name as hotel_name';
+            $res_idcode_record = $m_stock_record->alias('a')
+                ->field($idcode_fileds)
+                ->join('savor_finance_stock stock on a.stock_id=stock.id','left')
+                ->join('savor_hotel hotel on stock.hotel_id=hotel.id','left')
+                ->where(array('a.idcode'=>$idcode,'a.dstatus'=>1))
+                ->order('a.id desc')
+                ->limit('0,1')
+                ->select();
+
+            $sale_type = 0;
+            if(!empty($res_idcode_record[0]['out_stock_id'])){
+                if($res_idcode_record[0]['out_stock_id']==$v['in_stock_id']){
+                    $res_idcode_record[0]['out_stock_id'] = 0;
+                    $res_idcode_record[0]['out_stock_name'] = '';
+                    $res_idcode_record[0]['out_serial_number'] = '';
+                }
+
+                $now_idcode_data = array_merge($v,$res_idcode_record[0]);
+                if($res_idcode_record[0]['type']==7){
+                    $res_sale = $m_sale->getInfo(array('idcode'=>$idcode));
+                    $sale_type = $res_sale['type'];
+                    $settlement_price = $res_sale['settlement_price'];
+                }else{
+                    $res_sale = $m_sale->getAll('type,num,settlement_price',array('idcode'=>array('like',"%$idcode%")),0,1,'id desc');
+                    if(!empty($res_sale[0]['type'])){
+                        $sale_type = $res_sale[0]['type'];
+                        $settlement_price = $res_sale[0]['settlement_price']/$res_sale[0]['num'];
+                    }else{
+                        $hotel_id = intval($res_idcode_record[0]['hotel_id']);
+                        $settlement_price = $m_price_template_hotel->getHotelGoodsPrice($hotel_id,$v['goods_id']);
+                    }
+                }
+                $now_idcode_data['settlement_price'] = $settlement_price;
+                $now_idcode_data['sale_type'] = $sale_type;
+                if(empty($now_idcode_data['hotel_name'])){
+                    $now_idcode_data['hotel_name'] = '';
+                }
+                $m_tmpidcode->add($now_idcode_data);
+            }
+            echo "$idcode \r\n";
+        }
+        $now_time = date('Y-m-d H:i:s');
+        echo "idcode end $now_time \r\n";
+
+    }
 }
